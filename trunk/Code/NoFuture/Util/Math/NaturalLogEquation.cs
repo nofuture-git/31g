10304a8b01045d4d18a1389ecb6b2280d5e18795
NoFuture <brian.starrett@gmail.com>
2016-02-12 17:37:36 -0600

    added equation object for Ln

---
+++ trunk/Code/NoFuture/Util/Math/NaturalLogEquation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoFuture.Util.Math
+{
+    public class NaturalLogEquation
+    {
+        public double Intercept { get; set; }
+        public double Slope { get; set; }
+
+        public double SolveForY(double x)
+        {
+            return (Slope*(System.Math.Log(x))) + Intercept;
+        }
+
+        public double SolveForX(double y)
+        {
+            if (Slope == 0.0D)
+                return 0.0D;
+            var yy = y - Intercept;
+            yy = yy / Slope;
+            return System.Math.Pow(System.Math.E, yy);
+
+        }
+    }
+}
