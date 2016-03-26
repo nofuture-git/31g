8caa358c0ba4a8509fb89d8a8601ebce856b538f
NoFuture <brian.starrett@gmail.com>
2016-03-25 19:25:59 -0500

    misc notes from Machine Learning studies

---
+++ trunk/Notes/aima-csharp-3e/aima-csharp-3e/Learning/DataSet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aima_csharp_3e.Learning
+{
+
+    public interface IDataSet<T>
+    {
+        IEnumerable<IDecisionTree<T>> Examples { get; }
+        void AddExample(IDecisionTree<T> example);
+        int Size { get; }
+        IDecisionTree<T> this[int key] { get; }
+        IDataSet<T> RemoveExample(IDecisionTree<T> example);
+        T Target { get; }
+        IEnumerable<T> AttrNames { get; }
+
+    }
+
+    public class DataSetValueType : IDataSet<ValueType>
+    {
+        private readonly List<IDecisionTree<ValueType>> _examples;
+
+        public DataSetValueType(IEnumerable<IDecisionTree<ValueType>> examples)
+        {
+            _examples = examples.ToList();
+        }
+
+        public IEnumerable<IDecisionTree<ValueType>> Examples { get {return _examples;} }
+        public void AddExample(IDecisionTree<ValueType> example)
+        {
+            if (_examples.Any(x => x.Equals(example)))
+                return;
+            _examples.Add(example);
+        }
+
+        public int Size { get { return _examples.Count(); } }
+
+        public IDecisionTree<ValueType> this[int key]
+        {
+            get { return _examples.ToArray()[key]; }
+        }
+
+        public IDataSet<ValueType> RemoveExample(IDecisionTree<ValueType> example)
+        {
+            //TODO why is this needed?
+            throw new NotImplementedException();
+        }
+
+        public ValueType Target { get; }
+        public IEnumerable<ValueType> AttrNames { get; }
+    }
+}
---
+++ trunk/Notes/aima-csharp-3e/aima-csharp-3e/Learning/Learner.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aima_csharp_3e.Learning
+{
+    public interface ILearner<T>
+    {
+        void Train(IDataSet<T> ds);
+        T Predict(IDecisionTree<T> example);
+    }
+
+    public class DecisionTreeLearner : ILearner<ValueType>
+    {
+        private DecisionTreeValueType _tree;
+        private ValueType _dfVal;
+        private IDataSet<ValueType> _ds;
+        private IEnumerable<ValueType> _attrNames;
+
+        public DecisionTreeLearner(IDecisionTree<ValueType> tree, ValueType dfValue)
+        {
+            _tree = (DecisionTreeValueType)tree;
+            _dfVal = dfValue;
+        }
+
+
+        public void Train(IDataSet<ValueType> ds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueType Predict(IDecisionTree<ValueType> example)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual IDecisionTree<ValueType> DecisionTreeLearning(IEnumerable<DecisionTreeValueType> examples,
+            IEnumerable<ValueType> attributes, IEnumerable<DecisionTreeValueType> parentExamples = null)
+        {
+            var egs = examples.ToList();
+            var attrs = attributes.ToList();
+            if(egs.Count <= 0)
+                return PluralityValue(parentExamples);
+            if (egs.All(x => x.Classification == BooleanClassification.Negative))
+            {
+                return new DecisionTreeValueType {Classification = BooleanClassification.Negative};
+            }
+            if (egs.All(x => x.Classification == BooleanClassification.Positive))
+            {
+                return new DecisionTreeValueType {Classification = BooleanClassification.Positive};
+            }
+            if (attrs.Count <= 0)
+            {
+                return PluralityValue(egs);
+            }
+
+            var attrScores = attrs.Select(x => new Tuple<double, ValueType>(Importance(x, egs), x)).ToList();
+            var argMaxA = attrScores.Max(x => x.Item1);
+            var A = attrScores.First(x => x.Item1 == argMaxA).Item2;
+            
+
+            throw new NotImplementedException();
+        }
+
+        protected virtual IDecisionTree<ValueType> PluralityValue(IEnumerable<DecisionTreeValueType> df)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected double Importance(ValueType a, IEnumerable<DecisionTreeValueType> examples)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
