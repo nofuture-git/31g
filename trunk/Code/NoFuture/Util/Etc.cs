5d76a2803091c2d209cd305bb3e73957d9cedc3c
NoFuture <brian.starrett@gmail.com>
2016-10-06 17:10:17 -0500

    even more notes on Linear Algebra

--- trunk/Code/NoFuture/Util/Etc.cs
+++ trunk/Code/NoFuture/Util/Etc.cs
@@ -899,5 +899,80 @@ namespace NoFuture.Util
             var beforeOrOnT2 = t0.ComparedTo(t2) == ChronoCompare.Before || t0.ComparedTo(t2) == ChronoCompare.SameTime;
             return afterOrOnT1 && beforeOrOnT2;
         }
+
+        /// <summary>
+        /// The Jaro–Winkler distance (Winkler, 1990) is a measure of similarity between two strings.
+        /// </summary>
+        /// <param name="aString1"></param>
+        /// <param name="aString2"></param>
+        /// <param name="mWeightThreshold"></param>
+        /// <param name="mNumChars"></param>
+        /// <returns>1 means a perfect match, 0 means not match what-so-ever</returns>
+        /// <remarks>
+        /// [http://stackoverflow.com/questions/19123506/jaro-winkler-distance-algorithm-in-c-sharp]
+        /// </remarks>
+        public static double JaroWinklerDistance(this string aString1, string aString2, double mWeightThreshold = 0.7D,
+            int mNumChars = 4)
+        {
+            var lLen1 = aString1.Length;
+            var lLen2 = aString2.Length;
+            if (lLen1 == 0)
+                return lLen2 == 0 ? 1.0 : 0.0;
+
+            var lSearchRange = System.Math.Max(0, System.Math.Max(lLen1, lLen2)/2 - 1);
+
+            // default initialized to false
+            var lMatched1 = new bool[lLen1];
+            var lMatched2 = new bool[lLen2];
+
+            var lNumCommon = 0;
+            for (var i = 0; i < lLen1; ++i)
+            {
+                var lStart = System.Math.Max(0, i - lSearchRange);
+                var lEnd = System.Math.Min(i + lSearchRange + 1, lLen2);
+                for (var j = lStart; j < lEnd; ++j)
+                {
+                    if (lMatched2[j]) continue;
+                    if (aString1[i] != aString2[j])
+                        continue;
+                    lMatched1[i] = true;
+                    lMatched2[j] = true;
+                    ++lNumCommon;
+                    break;
+                }
+            }
+            if (lNumCommon == 0)
+                return 0.0;
+
+            var lNumHalfTransposed = 0;
+            var k = 0;
+            for (var i = 0; i < lLen1; ++i)
+            {
+                if (!lMatched1[i]) continue;
+                while (!lMatched2[k]) ++k;
+                if (aString1[i] != aString2[k])
+                    ++lNumHalfTransposed;
+                ++k;
+            }
+            // System.Diagnostics.Debug.WriteLine("numHalfTransposed=" + numHalfTransposed);
+            var lNumTransposed = lNumHalfTransposed/2;
+
+            // System.Diagnostics.Debug.WriteLine("numCommon=" + numCommon + " numTransposed=" + numTransposed);
+            double lNumCommonD = lNumCommon;
+            var lWeight = (lNumCommonD/lLen1
+                           + lNumCommonD/lLen2
+                           + (lNumCommon - lNumTransposed)/lNumCommonD)/3.0;
+
+            if (lWeight <= mWeightThreshold)
+                return lWeight;
+            var lMax = System.Math.Min(mNumChars, System.Math.Min(aString1.Length, aString2.Length));
+            var lPos = 0;
+            while (lPos < lMax && aString1[lPos] == aString2[lPos])
+                ++lPos;
+            if (lPos == 0)
+                return lWeight;
+            return lWeight + 0.1*lPos*(1.0 - lWeight);
+
+        }
     }
 }
