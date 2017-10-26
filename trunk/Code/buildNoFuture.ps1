ad6d30e38a9aebfc3f3119b9acc5b81259d5dc60
NoFuture <brian.starrett@gmail.com>
2017-10-26 17:21:23 -0500

    Fixed up the build script to run but it still targets the MSBuild version 14

--- trunk/Code/buildNoFuture.ps1
+++ trunk/Code/buildNoFuture.ps1
@@ -1,10 +1,16 @@
 ﻿Write-Host "Begin NoFuture build script" -ForegroundColor Yellow
 $currentMsBuildVer = "14"
-$targetDotNet = ".NET Framework 4.5"
+$targetDotNet = ".NET Framework 4.7"
 $installedDotNetFrameworks = @()
 $errors = @()
 
-#get .NET versions
+#get list of all programs as they appear in classic "Add\Remove Programs"
+Write-Host "Getting list of installed programs on this box (this may take awhile)." -ForegroundColor Yellow
+$installedPrograms = Get-WmiObject -Class Win32_Product
+
+#===================================
+#TEST FOR .NET FRAMEWORK
+Write-Host (New-Object System.String('-',80)) -ForegroundColor Green
 Write-Host "Getting installed version of Microsoft.NET." -ForegroundColor Yellow
 #https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed#ps_a
 $dotNet2ReleaseNum = @{
@@ -16,11 +22,11 @@ $dotNet2ReleaseNum = @{
 394802 = ".NET Framework 4.6.2";
 460798 = ".NET Framework 4.7";
 461308 = ".NET Framework 4.7.1";
+461310 = ".NET Framework 4.7.1";
 }
-Get-ChildItem "hklm:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\" | Get-ItemPropertyValue -Name Release | % {
- $installedDotNetFrameworks += $dotNet2ReleaseNum[$_]
+Get-ChildItem "hklm:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\" | Get-ItemProperty -Name Release | % {
+ $installedDotNetFrameworks += $dotNet2ReleaseNum[$_.Release]
 }
-
 
 Write-Host "The following .NET framework version (4.5 and up) where found." -ForegroundColor Yellow
 $installedDotNetFrameworks | % {
@@ -29,68 +35,140 @@ $installedDotNetFrameworks | % {
 
 $isTargetDotNetFrameworkInstalled = $installedDotNetFrameworks.Count -gt 0
 if(-not $isTargetDotNetFrameworkInstalled){
-    $errors += ".NET Frameword version $targetDotNet is not installed."
+    $errors += ".NET Framework version $targetDotNet is not installed. [REQUIRED]"
 }
+#===================================
 
-#get list of all programs as they appear in classic "Add\Remove Programs"
-Write-Host "Getting list of installed programs on this box." -ForegroundColor Yellow
-$installedPrograms = Get-WmiObject -Class Win32_Product
 
-#test for MSBuild
+#===================================
+#TEST FOR MSBUILD (pre VS 2017)
+Write-Host (New-Object System.String('-',80)) -ForegroundColor Green
 Write-Host "Testing for MSBuild ver. $currentMsBuildVer.0 at typical install location." -ForegroundColor Yellow
 $msBuild = "C:\Program Files (x86)\MSBuild\$currentMsBuildVer.0\Bin\MSBuild.exe"
 $isMsBuildInstalled = Test-Path $msBuild
 
 if(-not $isMsBuildInstalled){
-    $errors += "MSBuild ver. $currentMsBuildVer.0 was not found."
+    $errors += "MSBuild ver. $currentMsBuildVer.0 was not found. [REQUIRED]"
 }
+else{
+    Write-Host "MSBuild.exe found at:" -ForegroundColor Yellow
+    Write-Host ("`t$msBuild") -ForegroundColor Gray
+}
+#===================================
 
-#test for .NET core install
+#===================================
+#TEST FOR .NET CORE
+Write-Host (New-Object System.String('-',80)) -ForegroundColor Green
 Write-Host "Testing for .NET Core install" -ForegroundColor Yellow
-$isDotNetCoreInstalled = ($installedPrograms | ? {$_.Name -like "Microsoft .NET Core SDK*"}).Length -gt 0 `
-                         -or  ($env:Path.Split(";") | ? {$_ -eq "C:\Program Files\dotnet\"}).Length -gt 0
+$isDotNetCoreInstalled = $installedPrograms | ? {$_.Name -like "Microsoft .NET Core SDK*"}
 
 if(-not $isDotNetCoreInstalled){
-    $errors += "The Microsoft .NET Core SDK was not found"
+    $errors += "The Microsoft .NET Core SDK was not found. [NOT REQUIRED]"
 }
+else{
+    Write-Host "The Microsoft .NET Core SDK was found at:" -ForegroundColor Yellow
+    $isDotNetCoreInstalled | % {
+        Write-Host ("`t{0}" -f $_.Name ) -ForegroundColor Gray
+    }
+}
+#===================================
 
-#test for Java and JAVA_HOME -required for NoFuture.Antlr
+#===================================
+#TEST FOR JAVA
+Write-Host (New-Object System.String('-',80)) -ForegroundColor Green
 Write-Host "Testing for Java install (version 7 or higher)." -ForegroundColor Yellow
 $isJavaInstalled = $installedPrograms | ? {$_.Name -match "Java\(TM\) SE Development Kit [7-9]" -or `
                                              $_.Name -match "Java [7-9]" }
-
-Write-Host "Testing if the JAVA_HOME environment variable is set to a valid location." -ForegroundColor Yellow
-$javaHomeAssigned = [System.Environment]::GetEnvironmentVariable("JAVA_HOME","Machine")
+$javaHomeAssigned = [System.Environment]::GetEnvironmentVariable("JAVA_HOME")
 $isJavahomeAssigned = $javaHomeAssigned -ne $null -and (Test-Path $javaHomeAssigned)
-
-Write-Host "Testing if Java's \bin folder is present in the PATH environment variable." -ForegroundColor Yellow
-$isJavaInPath = ($env:Path.Split(";") | ? {$_ -like "*\java\bin*"}).Length -gt 0
+$isJavaInPath = $env:Path.Split(";") | ? {Test-Path (Join-Path $_ "java.exe")}
 
 if(-not $isJavaInstalled){
-    $errors += "Java ver. 7, 8, nor 9 were found."
+    $errors += "Java ver. 7, 8, nor 9 were found.  [NOT REQUIRED]"
+}
+else{
+    Write-Host "The following Java Versions were found:" -ForegroundColor Yellow
+    $isJavaInstalled | % {
+        Write-Host ("`t{0}" -f $_.Name ) -ForegroundColor Gray
+    }
 }
 if(-not $isJavahomeAssigned){
-    $errors += "JAVA_HOME environment variable is not set."
+    $errors += "JAVA_HOME environment variable is not set.  [NOT REQUIRED]"
+}
+else{
+    Write-Host "JAVA_HOME environment variable assigned to:" -ForegroundColor Yellow
+    Write-Host ("`t$javaHomeAssigned") -ForegroundColor Gray
 }
 if(-not $isJavaInPath){
-    $errors += "Java's \bin folder is not added to the global PATH environment variable."
+    $errors += "java.exe was not found in the global PATH environment variable.  [NOT REQUIRED]"
 }
-
-if(-not $isJavaInstalled -or -not $isJavahomeAssigned-or -not $isJavaInPath){
-    $errors += "`t`tThis is required to build NoFuture.Antlr (used to parse files and IL tokens)."
-    $errors += "`t`tThe rest of the NoFuture projects will work without this, but runtime errors are possible."
+else{
+    Write-Host "The following java.exe(s) were found in PATH environment variable:" -ForegroundColor Yellow
+    $isJavaInPath | % {
+        Write-Host "`t$_" -ForegroundColor Gray
+    }
 }
+#=================================
 
-#test for VC redist - required for DIA_SDK (pdb file parser)
-Write-Host "Testing for Microsoft Visual C++ (version $currentMsBuildVer) Redist. install." -ForegroundColor Yellow
-$isMsVc14Installed = $installedPrograms | ? {$_.Name -match "Microsoft Visual C\+\+ [2][0-9]{3} x(32|64) [^0-9]* $currentMsBuildVer\."}
+#===================================
+#TEST FOR C++
+Write-Host (New-Object System.String('-',80)) -ForegroundColor Green
+Write-Host "Testing for C++ Redist. install." -ForegroundColor Yellow
+$isMsVc14Installed = $installedPrograms | ? {$_.Name -match "Microsoft Visual C\+\+ [2][0-9]{3} x(32|64) (Minimum|Additional) Runtime [^0-9]*"}
 if(-not $isMsVc14Installed){
-    $errors += "Microsoft Visual C++ ... $currentMsBuildVer was not found."
-    $errors += "`t`tThis is required to build the 'DIA SDK' VC++ project (a .pdb file parser)."
-    $errors += "`t`tHowever, you may have no need of parsing .pdb files so it may be skipped."
+    $errors += "Microsoft Visual C++ [...] Runtime $currentMsBuildVer was not found. [NOT REQUIRED]"
 }
+else{
+    Write-Host "The following C++ Runtimes were found" -ForegroundColor Yellow
+    $isMsVc14Installed | % {
+        Write-Host ("`t{0}" -f $_.Name ) -ForegroundColor Gray
+    }
+}
+#===================================
 
-Write-Host "----" -ForegroundColor DarkGray
+#===================================
+#TEST FOR PYTHON
+Write-Host (New-Object System.String('-',80)) -ForegroundColor Green
+Write-Host "Testing for Python environments" -ForegroundColor Yellow
+$isPyPathSet = [System.Environment]::GetEnvironmentVariable("PYTHONPATH")
+$isPyHomeSet = [System.Environment]::GetEnvironmentVariable("PYTHONHOME")
+$isPythonInstalled = $installedPrograms | ? {$_.Name -match "Python [0-9\.]+ (Executables|Core|Standard)"}
+$isPythonInPath = $env:Path.Split(";") | ? {Test-Path (Join-Path $_ "python.exe")}
+if(-not $isPyPathSet){
+    $errors += "PYTHONPATH environment variable is not set. [NOT REQUIRED]"
+}
+else{
+    Write-Host "PYTHONPATH environment variable assigned to:" -ForegroundColor Yellow
+    Write-Host ("`t$isPyPathSet") -ForegroundColor Gray
+}
+if(-not $isPyHomeSet){
+    $errors += "PYTHONHOME environment variable is not set. [NOT REQUIRED]"
+}
+else{
+    Write-Host "PYTHONHOME environment variable assigned to:" -ForegroundColor Yellow
+    Write-Host ("`t$isPyHomeSet") -ForegroundColor Gray
+}
+if(-not $isPythonInstalled){
+    $errors += "No Python environments were found. [NOT REQUIRED]"
+}
+else{
+    Write-Host "The following Python environment(s) were found:" -ForegroundColor Yellow
+    $isPythonInstalled | % {
+        Write-Host ("`t{0}" -f $_.Name ) -ForegroundColor Gray
+    }
+}
+if(-not $isPythonInPath){
+    $errors += "python.exe was not found in the global PATH environment variable.  [NOT REQUIRED]"
+}
+else{
+    Write-Host "The following python.exe(s) were found in PATH environment variable:" -ForegroundColor Yellow
+    $isPythonInPath | % {
+        Write-Host "`t$_" -ForegroundColor Gray
+    }
+}
+#===================================
+
+Write-Host (New-Object System.String('-',80)) -ForegroundColor DarkGray
 
 foreach($err in $errors){
     Write-Host $err -ForegroundColor Cyan
@@ -104,12 +182,7 @@ if($errors.Length -gt 0){
     }
 }
 
-#Push-Location .\NoFuture\Antlr
-
-#& "$msBuild" NoFuture.Antlr.sln /t:build /tv:14.0 /m:8 /p:platform="any cpu" /p:configuration=debug /p:buildinparallel=true /p:CreateHardLinksForCopyFilesToOutputDirectoryIfPossible=true /p:CreateHardLinksForCopyAdditionalFilesIfPossible=true /p:CreateHardLinksForCopyLocalIfPossible=true /p:CreateHardLinksForPublishFilesIfPossible=true
-
-#Pop-Location
-
-#& "$msBuild" NoFuture.sln /t:build /tv:14.0 /m:8 /p:platform="any cpu" /p:configuration=debug /p:buildinparallel=true /p:CreateHardLinksForCopyFilesToOutputDirectoryIfPossible=true /p:CreateHardLinksForCopyAdditionalFilesIfPossible=true /p:CreateHardLinksForCopyLocalIfPossible=true /p:CreateHardLinksForPublishFilesIfPossible=true
+#TODO - MSBuild bound to Visual Studio now, to build with .NET Core is a lot of changes
+& "$msBuild" NoFuture.sln /t:build /tv:14.0 /m:8 /p:platform="any cpu" /p:configuration=debug /p:buildinparallel=true /p:CreateHardLinksForCopyFilesToOutputDirectoryIfPossible=true /p:CreateHardLinksForCopyAdditionalFilesIfPossible=true /p:CreateHardLinksForCopyLocalIfPossible=true /p:CreateHardLinksForPublishFilesIfPossible=true
 
 $r = Read-Host "press any key to exit..."
