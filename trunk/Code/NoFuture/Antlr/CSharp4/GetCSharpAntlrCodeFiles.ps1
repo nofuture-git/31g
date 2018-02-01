b26838a915285e021f1e54e9b225cafa73a370fc
NoFuture <brian.starrett@gmail.com>
2018-02-01 11:11:38 -0600

    moved the Antlr CSharp4 out to its own assembly

---
+++ trunk/Code/NoFuture/Antlr/CSharp4/GetCSharpAntlrCodeFiles.ps1
@@ -0,0 +1,12 @@
+﻿$myScriptLocation = Split-Path $PSCommandPath -Parent
+
+$antlrCsharpJar =  Resolve-Path (Join-Path $myScriptLocation "..\antlr4-csharp-4.6.4-complete.jar")
+
+$namespace = "NoFuture.Antlr.CSharp4"
+
+@("CSharp4PreProcessor.g4", "CSharp4.g4") | % {
+    $grammerFileFullName = Resolve-Path (Join-Path $myScriptLocation $_)
+
+    Invoke-Expression -Command "java -jar $antlrCsharpJar -listener -Dlanguage=CSharp_v4_5 -o $myScriptLocation -package $namespace $grammerFileFullName"
+}
+
