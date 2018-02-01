345613e70ae17c64c660f583dbecadb2aa02a8d6
NoFuture <brian.starrett@gmail.com>
2018-02-01 10:48:54 -0600

    moved Antlr HTML out to its own assembly

--- trunk/Code/NoFuture/Antlr/DotNetIlTypeName/GetCSharpAntlrCodeFiles.ps1
+++ trunk/Code/NoFuture/Antlr/DotNetIlTypeName/GetCSharpAntlrCodeFiles.ps1
@@ -4,8 +4,8 @@ $myScriptLocation = Split-Path $PSCommandPath -Parent
 
 $antlrCsharpJar =  Resolve-Path (Join-Path $myScriptLocation "..\antlr4-csharp-4.6.4-complete.jar")
 
-$namespace = "NoFuture.Antlr.{0}" -f ([System.IO.Path]::GetFileNameWithoutExtension($GrammerFileName))
+$namespace = "NoFuture.Antlr.DotNetIlTypeName"
 
 $grammerFileFullName = Resolve-Path (Join-Path $myScriptLocation $GrammerFileName)
 
-Invoke-Expression -Command "java -jar $antlrCsharpJar -listener -Dlanguage=CSharp_v4_5 -o $outputPath -package $namespace $grammerFileFullName"
+Invoke-Expression -Command "java -jar $antlrCsharpJar -listener -Dlanguage=CSharp_v4_5 -o $myScriptLocation -package $namespace $grammerFileFullName"
---
+++ trunk/Code/NoFuture/Antlr/HTML/GetCSharpAntlrCodeFiles.ps1
@@ -0,0 +1,12 @@
+﻿$myScriptLocation = Split-Path $PSCommandPath -Parent
+
+$antlrCsharpJar =  Resolve-Path (Join-Path $myScriptLocation "..\antlr4-csharp-4.6.4-complete.jar")
+
+$namespace = "NoFuture.Antlr.HTML"
+
+@("HTMLLexer.g4", "HTMLParser.g4") | % {
+    $grammerFileFullName = Resolve-Path (Join-Path $myScriptLocation $_)
+
+    Invoke-Expression -Command "java -jar $antlrCsharpJar -listener -Dlanguage=CSharp_v4_5 -o $myScriptLocation -package $namespace $grammerFileFullName"
+}
+
