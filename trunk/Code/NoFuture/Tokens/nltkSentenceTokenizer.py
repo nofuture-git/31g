1784dfe7af2fa1afb4962e582150a1b5d4734f94
NoFuture <brian.starrett@gmail.com>
2018-12-18 14:17:05 -0600

    added a py script to parse text files into sentences

---
+++ trunk/Code/NoFuture/Tokens/nltkSentenceTokenizer.py
@@ -0,0 +1,36 @@
+"""
+Simple hook python script to pass some text file
+to the sentence tokenizer of NLTK package.
+To install the package:
+ C:\PS> python -m pip install nltk
+
+To run the script (from PowerShell on Windows):
+ C:\PS> $thisScript = "C:\Projects\31g\trunk\Code\NoFuture\Tokens\nltkSentenceTokenizer.py"
+ C:\PS> $myTextFile = C:\Temp\someTextFile.txt
+ C:\PS> $tokenFile = python $thisScript $myTextFile
+"""
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+import sys
+import os
+
+#get the file path from the invocation 
+fullFilePath = sys.argv[1]
+(filePath, fileName) = os.path.split(fullFilePath)
+(fileNameLessExt, fileExt) = os.path.splitext(fileName)
+fullOutFile = os.path.join(filePath, fileNameLessExt + ".nltk" + fileExt)
+
+txtFile = open(fullFilePath, 'r+')
+tokenFile = open(fullOutFile, 'w')
+
+txtLines = txtFile.readlines()
+tokenLines = []
+for txtLn in txtLines:
+    for tokenSen in sent_tokenize(txtLn):
+        tokenLines.append(tokenSen + "\n")
+
+tokenFile.writelines(tokenLines)
+tokenFile.flush()
+tokenFile.close()
+txtFile.close()
+print(fullOutFile)
