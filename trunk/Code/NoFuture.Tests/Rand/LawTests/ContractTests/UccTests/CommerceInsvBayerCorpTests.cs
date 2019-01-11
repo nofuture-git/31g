2e30f16bc2549c512cdcd708cd8d9a9802055c4a
NoFuture <brian.starrett@gmail.com>
2019-01-11 14:05:51 -0600

    in Nf.Rand.Law, dropped the TermInConductOnly.cs since its not actually a definable term but is a condition on which only agreed terms are applicable.

--- trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/UccTests/CommerceInsvBayerCorpTests.cs
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/UccTests/CommerceInsvBayerCorpTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Contracts.Ucc;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 
@@ -23,17 +24,75 @@ namespace NoFuture.Rand.Tests.LawTests.ContractTests.UccTests
         [Test]
         public void CommerceInsvBayerCorp()
         {
+            var testSubject = new Agreement();
+            testSubject.IsApprovalExpressed = lp => true;
+            testSubject.TermsOfAgreement = lp =>
+            {
+                var commerceIns = lp as CommerceIns;
+                if (commerceIns != null)
+                    return commerceIns.GetTerms();
+                var bayer = lp as BayerCorp;
+                if (bayer != null)
+                    return bayer.GetTerms();
+                return new HashSet<Term<object>>();
+            };
+            var testResult = testSubject.GetAgreedTerms(new CommerceIns(), new BayerCorp());
+            Assert.IsNotNull(testResult);
+            Assert.IsTrue(testResult.Count == 1);
+            Assert.IsTrue(testResult.Any(t => t.Name == "bulk nylon fiber"));
+        }
 
+        [Test]
+        public void TestGetDistinctTerms()
+        {
+            var lp00 = new CommerceIns();
+            var lp01 = new BayerCorp();
+            var testSubject = new Agreement
+            {
+                IsApprovalExpressed = lp => true,
+                TermsOfAgreement = lp =>
+                {
+                    var commerceIns = lp as CommerceIns;
+                    if (commerceIns != null)
+                        return commerceIns.GetTerms();
+                    var bayer = lp as BayerCorp;
+                    if (bayer != null)
+                        return bayer.GetTerms();
+                    return new HashSet<Term<object>>();
+                }
+            };
+            var testResult = testSubject.GetDistinctTerms(lp00, lp01);
+            Assert.IsNotNull(testResult);
         }
     }
 
     public class CommerceIns : LegalPerson
     {
         public CommerceIns() : base("COMMERCE & INDUSTRY INS. CO.") { }
+
+        public ISet<Term<object>> GetTerms()
+        {
+            var terms = new HashSet<Term<object>>();
+            terms.Add(new Term<object>("arbitration provision", new object()));
+            terms.Add(new Term<object>("purchase order", new object()));
+            terms.Add(new Term<object>("bulk nylon fiber", DBNull.Value));
+
+            return terms;
+        }
     }
 
     public class BayerCorp : LegalPerson
     {
         public BayerCorp() : base("BAYER CORP.") { }
+
+        public ISet<Term<object>> GetTerms()
+        {
+            var terms = new HashSet<Term<object>>();
+            terms.Add(new Term<object>("invoice", new object()));
+            terms.Add(TermExpresslyConditional.Value);
+            terms.Add(new Term<object>("bulk nylon fiber", DBNull.Value));
+
+            return terms;
+        }
     }
 }
