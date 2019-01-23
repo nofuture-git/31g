6febd213304baaf8c0d625c74bff6b4c1a421e20
NoFuture <brian.starrett@gmail.com>
2019-01-23 11:21:54 -0600

    added new type ContractTerm in Nf.Rand.Law.Contracts to capture the idea that terms can have different sources

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/ContractTermTests.cs
@@ -0,0 +1,29 @@
+﻿using NoFuture.Rand.Law.US.Contracts;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Tests.LawTests.ContractTests
+{
+    [TestFixture]
+    public class ContractTermTests
+    {
+        [Test]
+        public void TestCompareTo()
+        {
+            var testSubject00 = new ContractTerm<object>("car parts", "car parts");
+            var testSubject01 = new ContractTerm<object>("car parts", "car parts");
+
+            var testResult = testSubject00.CompareTo(null);
+            Assert.AreEqual(1, testResult);
+
+            testResult = testSubject00.CompareTo(testSubject01);
+            Assert.AreEqual(0, testResult);
+
+            testSubject00.Source = TermSource.Written;
+            testResult = testSubject00.CompareTo(testSubject01);
+            Assert.AreEqual(-1, testResult);
+
+            testResult = testSubject01.CompareTo(testSubject00);
+            Assert.AreEqual(1, testResult);
+        }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/TermsTests/MitchillvLathTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Contracts;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace NoFuture.Rand.Tests.LawTests.ContractTests.TermsTests
+{
+    /// <summary>
+    /// MITCHILL v. LATH Court of Appeals of New York 247 N.Y. 377, 160 N.E. 646 (1928)
+    /// </summary>
+    [TestFixture]
+    public class MitchillvLathTests
+    {
+        [Test]
+        public void MitchillvLath()
+        {
+
+        }
+
+        public ISet<Term<object>> GetTerms()
+        {
+            var terms = new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("purchase farm", 1) {Source = TermSource.Written},
+                new ContractTerm<object>("remove ice house", 2) {Source = TermSource.Oral}
+            };
+            return terms;
+        }
+    }
+
+    public class OfferPurchaseFarmRemIceHouse : Promise
+    {
+        public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
+        {
+            return (offeror is Mitchill || offeror is Lath)
+                   && (offeree is Mitchill || offeree is Lath);
+        }
+    }
+
+    public class AcceptancePurchaseFarmRemIceHouse : OfferPurchaseFarmRemIceHouse { }
+
+    public class Mitchill : LegalPerson
+    {
+
+    }
+
+    public class Lath : LegalPerson
+    {
+
+    }
+}
