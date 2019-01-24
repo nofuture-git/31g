624eed8b66073fb0d251ad05fb6e28a3fbe0dd25
NoFuture <brian.starrett@gmail.com>
2019-01-24 16:35:02 -0600

    added a type in Nf.Rand.Law.Us.Contracts.Litigate to deal with conflicted meaning of words, added tests thereof

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/LitigateTests/InReEstateOfSoperTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFuture.Rand.Core.Enums;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Contracts;
+using NoFuture.Rand.Law.US.Contracts.Litigate;
+using NoFuture.Rand.Law.US.Contracts.Terms;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace NoFuture.Rand.Tests.LawTests.ContractTests.LitigateTests
+{
+    /// <summary>
+    /// IN RE ESTATE OF SOPER Supreme Court of Minnesota 196 Minn. 60, 264 N.W. 427 (1935)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, what does the court do when a common use word has two possible meanings
+    /// ]]>
+    /// </remarks>
+    [TestFixture()]
+    public class InReEstateOfSoperTests
+    {
+        [Test]
+        public void InReEstateOfSoper()
+        {
+            var testContract = new ComLawContract<Promise>
+            {
+                Offer = new InheritSoperEstate(),
+                Acceptance = o => o is InheritSoperEstate ? new GetsSoperEstate() : null,
+                Assent = new MutualAssent
+                {
+                    IsApprovalExpressed = lp => true,
+                    TermsOfAgreement = lp =>
+                    {
+                        switch (lp)
+                        {
+                            case IraSoper _:
+                                return ((IraSoper)lp).GetTerms();
+                            case GertrudeWhitby _:
+                                return ((GertrudeWhitby)lp).GetTerms();
+                            case AdelineWestphal _:
+                                return ((AdelineWestphal)lp).GetTerms();
+                            default:
+                                return null;
+                        }
+                    }
+                }
+            };
+
+            testContract.Consideration = new Consideration<Promise>(testContract)
+            {
+                IsGivenByPromisee = (lp, p) => true,
+                IsSoughtByPromisor = (lp, p) => true
+            };
+
+            //although legally his Gertrude is his legal wife - the contract's intend was obviously Adeline
+            var testSubject = new SemanticDilemma<Promise>(testContract)
+            {
+                IsPreferred = t => t.RefersTo is AdelineWestphal
+            };
+
+            var testResult = testSubject.IsValid(new IraSoper(), new GertrudeWhitby());
+            Assert.IsTrue(testResult);
+            testResult = testSubject.IsValid(new IraSoper(), new AdelineWestphal());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsFalse(testResult);
+        }
+    }
+
+    public class InheritSoperEstate : Promise
+    {
+        public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
+        {
+            return (offeror is IraSoper) && (offeree is GertrudeWhitby || offeree is AdelineWestphal);
+        }
+    }
+
+    public class GetsSoperEstate : InheritSoperEstate { }
+
+    public class IraSoper : LegalPerson
+    {
+        public IraSoper() : base("John Young")
+        {
+            Names.Add(new Tuple<KindsOfNames, string>(KindsOfNames.Former, "Ira Soper"));
+        }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("wife", new AdelineWestphal(), new WrittenTerm(), new ExpressTerm()),
+            };
+        }
+
+    }
+
+    public class GertrudeWhitby : LegalPerson
+    {
+        public GertrudeWhitby() : base("Gertrude Whitby") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("wife", this, new WrittenTerm(), new ExpressTerm()),
+            };
+        }
+    }
+
+    public class AdelineWestphal : LegalPerson
+    {
+        public AdelineWestphal() : base("Adeline Westphal") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("wife", this, new WrittenTerm(), new ExpressTerm()),
+            };
+        }
+    }
+
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/LitigateTests/WwwAssociatesvGiancontieriTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Contracts;
+using NoFuture.Rand.Law.US.Contracts.Litigate;
+using NoFuture.Rand.Law.US.Contracts.Terms;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Tests.LawTests.ContractTests.LitigateTests
+{
+    /// <summary>
+    /// W.W.W. ASSOCIATES, INC. v. GIANCONTIERI Court of Appeals of New York 77 N.Y.2d 157, 566 N.E.2d 639, 565 N.Y.S.2d 440 (1990)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, the expressly conditional term may exclude terms that are are written but ancillary
+    /// "extrinsic and parol evidence is not admissible to create an ambiguity in a written agreement which is complete and clear and unambiguous upon its face."
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class WwwAssociatesvGiancontieriTests
+    {
+        [Test]
+        public void WwwAssociatesvGiancontieri()
+        {
+            var testContract = new ComLawContract<Promise>
+            {
+                Offer = new OfferWwwAssociates(),
+                Acceptance = o => o is OfferWwwAssociates ? new AcceptanceWwwAssociates() : null,
+                Assent = new MutualAssent
+                {
+                    IsApprovalExpressed = lp => true,
+                    TermsOfAgreement = lp =>
+                    {
+                        switch (lp)
+                        {
+                            case WwwAssociates _:
+                                return ((WwwAssociates)lp).GetTerms();
+                            case Giancontieri _:
+                                return ((Giancontieri)lp).GetTerms();
+                            default:
+                                return null;
+                        }
+                    }
+                }
+            };
+
+            testContract.Consideration = new Consideration<Promise>(testContract)
+            {
+                IsGivenByPromisee = (lp, p) => true,
+                IsSoughtByPromisor = (lp, p) => true
+            };
+
+            var testSubject = new SemanticDilemma<Promise>(testContract)
+            {
+                //since this was present
+                IsPrerequisite = t => t is ExpresslyConditionalTerm,
+                IsPreferred = t =>
+                {
+                    // and the contract clear said "either party can cancel" after a specific date
+                    var isDateTime = t.RefersTo is DateTime;
+                    if (!isDateTime)
+                        return false;
+                    var tDt = (DateTime) t.RefersTo;
+                    return tDt != DateTime.MaxValue;
+                }
+            };
+            var testResult = testSubject.IsValid(new WwwAssociates(), new Giancontieri());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+
+        }
+    }
+
+    public class OfferWwwAssociates : Promise
+    {
+        public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
+        {
+            return (offeror is WwwAssociates || offeror is Giancontieri)
+                   && (offeree is WwwAssociates || offeree is Giancontieri);
+        }
+    }
+
+    public class AcceptanceWwwAssociates : OfferWwwAssociates { }
+
+    public class WwwAssociates : LegalPerson
+    {
+        public WwwAssociates(): base("W.W.W. ASSOCIATES, INC.") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                ExpresslyConditionalTerm.Value,
+                new ContractTerm<object>("cancel this contract", DateTime.MaxValue, new WrittenTerm(), new ExpressTerm()),
+            };
+        }
+        public bool WantsToCancelContractAfterDate => false;
+    }
+
+    public class Giancontieri : LegalPerson
+    {
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                ExpresslyConditionalTerm.Value,
+                new ContractTerm<object>("cancel this contract", new DateTime(1987,6,1), new WrittenTerm(), new ExpressTerm())
+            };
+        }
+
+        public bool WantsToCancelContractAfterDate => true;
+    }
+}
