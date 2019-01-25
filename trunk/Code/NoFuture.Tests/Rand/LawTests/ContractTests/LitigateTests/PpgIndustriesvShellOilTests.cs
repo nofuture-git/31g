caf294e90e3310c136958bf7cbda17061e33f348
NoFuture <brian.starrett@gmail.com>
2019-01-25 10:25:15 -0600

    added type in Nf.Rand.Law.Us.Contracts.Litigate to deal with another kind of dilemma and added a base type for both, also added unit tests thereof

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/LitigateTests/PpgIndustriesvShellOilTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Contracts;
+using NoFuture.Rand.Law.US.Contracts.Terms;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System.Linq.Expressions;
+using NoFuture.Rand.Law.US.Contracts.Litigate;
+
+namespace NoFuture.Rand.Tests.LawTests.ContractTests.LitigateTests
+{
+    [TestFixture()]
+    public class PpgIndustriesvShellOilTests
+    {
+        [Test]
+        public void PpgIndustriesvShellOil()
+        {
+            var testContract = new ComLawContract<Promise>
+            {
+                Offer = new OfferSellEthylene(),
+                Acceptance = o => o is OfferSellEthylene ? new AcceptanceSellEthylene() : null,
+                Assent = new MutualAssent
+                {
+                    IsApprovalExpressed = lp => true,
+                    TermsOfAgreement = lp =>
+                    {
+                        switch (lp)
+                        {
+                            case PpgIndustries _:
+                                return ((PpgIndustries)lp).GetTerms();
+                            case ShellOil _:
+                                return ((ShellOil)lp).GetTerms();
+                            default:
+                                return null;
+                        }
+                    }
+                }
+            };
+
+            testContract.Consideration = new Consideration<Promise>(testContract)
+            {
+                IsGivenByPromisee = (lp, p) => true,
+                IsSoughtByPromisor = (lp, p) => true
+            };
+
+            var testSubject = new SyntacticDilemma<Promise>(testContract)
+            {
+                IsIntendedComposition = terms => terms != null && terms.Any(t => t.Name == "OR"),
+            };
+
+            var testResult = testSubject.IsValid(new PpgIndustries(), new ShellOil());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class OfferSellEthylene : Promise
+    {
+        public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
+        {
+            return (offeror is PpgIndustries || offeror is ShellOil)
+                   && (offeree is PpgIndustries || offeree is ShellOil);
+        }
+    }
+
+    public class AcceptanceSellEthylene : OfferSellEthylene { }
+
+    public class PpgIndustries : LegalPerson
+    {
+        public PpgIndustries(): base("PPG INDUSTRIES, INC.") {}
+
+        public ISet<Term<object>> GetTerms()
+        {
+            var trueConst = Expression.Constant(true);
+            var falseConst = Expression.Constant(false);
+            var xorExpr = Expression.ExclusiveOr(trueConst, falseConst);
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("reasonably beyond its control", 1, new ExpressTerm(), new WrittenTerm()),
+                new ContractTerm<object>("XOR", xorExpr, new ExpressTerm(), new WrittenTerm()),
+                new ContractTerm<object>("by fire, explosion [...]", 2, new ExpressTerm(), new WrittenTerm())
+            };
+        }
+    }
+
+    public class ShellOil : LegalPerson
+    {
+        public ShellOil():base("SHELL OIL CO.") {}
+        public ISet<Term<object>> GetTerms()
+        {
+            var trueConst = Expression.Constant(true);
+            var falseConst = Expression.Constant(false);
+            var orExpr = Expression.Or(trueConst, falseConst);
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("reasonably beyond its control", 1, new ExpressTerm(), new WrittenTerm()),
+                new ContractTerm<object>("OR", orExpr, new ExpressTerm(), new WrittenTerm()),
+                new ContractTerm<object>("by fire, explosion [...]", 2, new ExpressTerm(), new WrittenTerm())
+            };
+        }
+
+    }
+}
