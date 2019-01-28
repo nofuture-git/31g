7c1bbd35aa860579db623af2a4b8c02ce68b73f4
NoFuture <brian.starrett@gmail.com>
2019-01-28 15:04:56 -0600

    changed inheritance tree of various Contract Terms to extend the implied term and added a new one for the concept of Default

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/LitigateTests/MendenhallvHanesbrandsTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Contracts;
+using NoFuture.Rand.Law.US.Contracts.Terms;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Tests.LawTests.ContractTests.LitigateTests
+{
+    /// <summary>
+    /// MENDENHALL v. HANESBRANDS, INC. U.S.District Court for the Middle District of North Carolina 856 F.Supp. 2d 717 (M.D.N.C. 2012)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, default implied terms of good faith and fair dealings 
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class MendenhallvHanesbrandsTests
+    {
+        [Test]
+        public void MendenhallvHanesBrands()
+        {
+            var testContract = new ComLawContract<Promise>
+            {
+                Offer = new OfferPromoteChampion(),
+                Acceptance = o => o is OfferPromoteChampion ? new AcceptancePromoteChampion() : null,
+                Assent = new MutualAssent
+                {
+                    IsApprovalExpressed = lp => true,
+                    TermsOfAgreement = lp =>
+                    {
+                        switch (lp)
+                        {
+                            case Mendenhall _:
+                                return ((Mendenhall)lp).GetTerms();
+                            case HanesBrands _:
+                                return ((HanesBrands)lp).GetTerms();
+                            default:
+                                return null;
+                        }
+                    }
+                }
+            };
+
+            testContract.Consideration = new Consideration<Promise>(testContract)
+            {
+                IsGivenByPromisee = (lp, p) => true,
+                IsSoughtByPromisor = (lp, p) => true
+            };
+
+            var testResult = testContract.IsValid(new Mendenhall(), new HanesBrands());
+            Console.WriteLine(testContract.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class OfferPromoteChampion : Promise
+    {
+        public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
+        {
+            return (offeror is Mendenhall || offeror is HanesBrands)
+                && (offeree is Mendenhall || offeree is HanesBrands);
+        }
+    }
+
+    public class AcceptancePromoteChampion : OfferPromoteChampion
+    {
+    }
+
+    public class Mendenhall : LegalPerson
+    {
+        public Mendenhall() : base("RASHARD MENDENHALL") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("morals clause", DBNull.Value),
+                new ContractTerm<object>("covenant of good faith", 2)
+            };
+        }
+    }
+
+    public class HanesBrands : LegalPerson
+    {
+        public HanesBrands() : base("HANESBRANDS, INC.") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("morals clause", DBNull.Value),
+                new ContractTerm<object>("covenant of good faith", 2)
+            };
+        }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/LitigateTests/WoodvLucyLadyTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Contracts;
+using NoFuture.Rand.Law.US.Contracts.Terms;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Tests.LawTests.ContractTests.LitigateTests
+{
+    /// <summary>
+    /// WOOD v. LUCY, LADY DUFF-GORDON Court of Appeals of New York 222 N.Y. 88, 118 N.E. 214 (1917)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, the balancing part of consideration may itself be implied
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class WoodvLucyLadyTests
+    {
+        [Test]
+        public void WoodvLucyLady()
+        {
+            var testContract = new ComLawContract<Promise>
+            {
+                Offer = new OfferTurnThisVogueIntoMoney(),
+                Acceptance = o => o is OfferTurnThisVogueIntoMoney ? new AcceptanceTurnThisVogueIntoMoney() : null,
+                Assent = new MutualAssent
+                {
+                    IsApprovalExpressed = lp => true,
+                    TermsOfAgreement = lp =>
+                    {
+                        switch (lp)
+                        {
+                            case Wood _:
+                                return ((Wood)lp).GetTerms();
+                            case LucyLady _:
+                                return ((LucyLady)lp).GetTerms();
+                            default:
+                                return null;
+                        }
+                    }
+                }
+            };
+
+            testContract.Consideration = new Consideration<Promise>(testContract)
+            {
+                IsGivenByPromisee = (lp, p) => true,
+                IsSoughtByPromisor = (lp, p) => true
+            };
+
+            var testResult = testContract.IsValid(new Wood(), new LucyLady());
+            Console.WriteLine(testContract.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class OfferTurnThisVogueIntoMoney : Promise
+    {
+        public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
+        {
+            return (offeror is Wood || offeror is LucyLady)
+                && (offeree is Wood || offeree is LucyLady);
+        }
+    }
+
+    public class AcceptanceTurnThisVogueIntoMoney : OfferTurnThisVogueIntoMoney { }
+
+    public class Wood : LegalPerson
+    {
+        public Wood() : base("OTIS F. WOOD") { }
+
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("exclusive right", DBNull.Value),
+                new ContractTerm<object>("reasonable efforts to bring profits and revenues into existence", 2, new ImpliedTerm()),
+            };
+        }
+    }
+
+    public class LucyLady : LegalPerson
+    {
+        public LucyLady() : base("LUCY, LADY DUFF-GORDON") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("exclusive right", DBNull.Value, new ExpressTerm(), new WrittenTerm()),
+                new ContractTerm<object>("reasonable efforts to bring profits and revenues into existence", 2, new ImpliedTerm()),
+            };
+        }
+    }
+}
