3071927b2d70f69d4c0fc20a638dda8a1db3256b
NoFuture <brian.starrett@gmail.com>
2019-01-29 17:14:09 -0600

    added another unit test to deal with breach in UCC context

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/BreachTests/KcaElectronicsvLegacyElectronicsTests.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Contracts;
+using NoFuture.Rand.Law.US.Contracts.Breach;
+using NoFuture.Rand.Law.US.Contracts.Terms;
+using NoFuture.Rand.Law.US.Contracts.Ucc;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Tests.LawTests.ContractTests.BreachTests
+{
+    /// <summary>
+    /// KCA ELECTRONICS, INC. v. LEGACY ELECTRONICS, INC. Court of Appeal of California, Fourth Appellate District 2007 Cal.App.Unpub.LEXIS 6107 (Ct.App.)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// 
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class KcaElectronicsvLegacyElectronicsTests
+    {
+        [Test]
+        public void KcaElectronicsvLegacyElectronics()
+        {
+            var testContract = new UccContract<Goods>
+            {
+                Offer = new OfferSellSolderBalls(),
+                Acceptance = o => o is OfferSellSolderBalls ? new AcceptanctSellSolderBalls() : null,
+                Assent = new MutualAssent
+                {
+                    IsApprovalExpressed = lp => true,
+                    TermsOfAgreement = lp =>
+                    {
+                        switch (lp)
+                        {
+                            case KcaElectronics _:
+                                return ((KcaElectronics)lp).GetTerms();
+                            case LegacyElectronics _:
+                                return ((LegacyElectronics)lp).GetTerms();
+                            default:
+                                return null;
+                        }
+                    }
+                },
+                IsInstallmentContract = true
+            };
+
+            var testResult = testContract.IsValid(new KcaElectronics(), new LegacyElectronics());
+            Assert.IsTrue(testResult);
+
+            var whatKcaShipped = new OfferSellSolderBalls
+            {
+                IsCopperExposed = true,
+                IsSmashed = true,
+                IsUndersized = true,
+                IsCoveredWithStickyTapeResidue = true,
+                PercentOfDefective = 0.06f
+            };
+
+            var testSubject = new PerfectTender<Goods>(testContract)
+            {
+                ActualPerformance = lp =>
+                {
+                    if (lp is KcaElectronics)
+                        return whatKcaShipped;
+                    if (lp is LegacyElectronics)
+                        return new AcceptanctSellSolderBalls();
+                    return null;
+                }
+            };
+
+            testResult = testSubject.IsValid(new KcaElectronics(), new LegacyElectronics());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsFalse(testResult);
+        }
+    }
+
+    public class OfferSellSolderBalls : Goods
+    {
+        public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
+        {
+            return (offeror is KcaElectronics || offeror is LegacyElectronics)
+                   && (offeree is KcaElectronics || offeree is LegacyElectronics);
+        }
+
+        public bool IsCoveredWithStickyTapeResidue { get; set; } = false;
+        public bool IsUndersized { get; set; } = false;
+        public bool IsSmashed { get; set; } = false;
+        public bool IsCopperExposed { get; set; } = false;
+        public float PercentOfDefective { get; set; } = 0.0099f;
+
+        public bool IsSubstantialImpairment => PercentOfDefective > 0.01f;
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as OfferSellSolderBalls;
+            if (o == null)
+                return false;
+            return o.IsCoveredWithStickyTapeResidue == IsCoveredWithStickyTapeResidue
+                   && o.IsUndersized == IsUndersized
+                   && o.IsSmashed == IsSmashed
+                   && o.IsCopperExposed == IsCopperExposed
+                ;
+        }
+
+        public override bool EquivalentTo(object obj)
+        {
+            var o = obj as OfferSellSolderBalls;
+            if (o == null)
+                return false;
+            return o.IsSubstantialImpairment == IsSubstantialImpairment;
+        }
+    }
+
+    public class AcceptanctSellSolderBalls : OfferSellSolderBalls
+    {
+        public override bool Equals(object obj)
+        {
+            var o = obj as AcceptanctSellSolderBalls;
+            if (o == null)
+                return false;
+            return true;
+        }
+    }
+
+    public class KcaElectronics : Merchant
+    {
+        public KcaElectronics(): base("KCA ELECTRONICS, INC.") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("solder balls", DBNull.Value),
+            };
+        }
+
+        public override bool IsSkilledOrKnowledgeableOf(Goods goods)
+        {
+            return true;
+        }
+    }
+
+    public class LegacyElectronics : Merchant
+    {
+        public LegacyElectronics(): base("LEGACY ELECTRONICS, INC.") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("solder balls", DBNull.Value),
+            };
+        }
+
+        public override bool IsSkilledOrKnowledgeableOf(Goods goods)
+        {
+            return true;
+        }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/Get-ContractsUnitTestTemplate.ps1
@@ -0,0 +1,124 @@
+﻿function Get-ContractsUnitTestTemplate
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true,position=0)]
+        [string] $Plaintiff,
+        [Parameter(Mandatory=$true,position=1)]
+        [string] $Defendant
+    )
+    Process
+    {
+$someCode = @"
+using System;
+using System.Collections.Generic;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Contracts;
+using NoFuture.Rand.Law.US.Contracts.Terms;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Tests.LawTests.ContractTests.BreachTests
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// 
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class ${Plaintiff}v${Defendant}Tests
+    {
+        [Test]
+        public void ${Plaintiff}v${Defendant}()
+        {
+            var testContract = new ComLawContract<Promise>
+            {
+                Offer = new Offer_RenameMe(),
+                Acceptance = o => o is Offer_RenameMe ? new Acceptanct_RenameMe() : null,
+                Assent = new MutualAssent
+                {
+                    IsApprovalExpressed = lp => true,
+                    TermsOfAgreement = lp =>
+                    {
+                        switch (lp)
+                        {
+                            case ${Plaintiff} _:
+                                return ((${Plaintiff})lp).GetTerms();
+                            case ${Defendant} _:
+                                return ((${Defendant})lp).GetTerms();
+                            default:
+                                return null;
+                        }
+                    }
+                }
+            };
+
+            testContract.Consideration = new Consideration<Promise>(testContract)
+            {
+                IsGivenByPromisee = (lp, p) => true,
+                IsSoughtByPromisor = (lp, p) => true
+            };
+        }
+    }
+
+    public class Offer_RenameMe : Promise
+    {
+        public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
+        {
+            return (offeror is ${Plaintiff} || offeror is ${Defendant})
+                   && (offeree is ${Plaintiff} || offeree is ${Defendant});
+        }
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as Offer_RenameMe;
+            if (o == null)
+                return false;
+            return true;
+        }
+    }
+
+    public class Acceptanct_RenameMe : Offer_RenameMe
+    {
+        public override bool Equals(object obj)
+        {
+            var o = obj as Acceptanct_RenameMe;
+            if (o == null)
+                return false;
+            return true;
+        }
+    }
+
+    public class ${Plaintiff} : LegalPerson
+    {
+        public ${Plaintiff}(): base("") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("", DBNull.Value),
+            };
+        }
+    }
+
+    public class ${Defendant} : LegalPerson
+    {
+        public ${Defendant}(): base("") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("", DBNull.Value),
+            };
+        }
+    }
+}
+
+"@
+        $filename = Join-Path (Get-Location).Path  ".\${Plaintiff}v${Defendant}Tests.cs"
+        [System.IO.File]::WriteAllText($filename, $someCode, [System.Text.Encoding]::UTF8)
+    }
+}
