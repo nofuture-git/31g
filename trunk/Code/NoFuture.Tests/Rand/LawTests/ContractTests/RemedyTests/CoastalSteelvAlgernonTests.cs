489c9a3dcf0274d7eb187d8f2c888b112cb12338
NoFuture <brian.starrett@gmail.com>
2019-02-01 14:21:34 -0600

    added a new type for a kind Restitution

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/RemedyTests/CoastalSteelvAlgernonTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Contracts;
+using NoFuture.Rand.Law.US.Contracts.Terms;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Tests.LawTests.ContractTests.BreachTests
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// 
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class CoastalSteelvAlgernonTests
+    {
+        [Test]
+        public void CoastalSteelvAlgernon()
+        {
+            var testContract = new ComLawContract<Promise>
+            {
+                Offer = new Offer_RenameMe(),
+                Acceptance = o => o is Offer_RenameMe ? new Acceptanct_RenameMe() : null,
+                Assent = new MutualAssent
+                {
+                    IsApprovalExpressed = lp => true,
+                    TermsOfAgreement = lp =>
+                    {
+                        switch (lp)
+                        {
+                            case CoastalSteel _:
+                                return ((CoastalSteel)lp).GetTerms();
+                            case Algernon _:
+                                return ((Algernon)lp).GetTerms();
+                            default:
+                                return null;
+                        }
+                    }
+                }
+            };
+
+            testContract.Consideration = new Consideration<Promise>(testContract)
+            {
+                IsGivenByPromisee = (lp, p) => true,
+                IsSoughtByPromisor = (lp, p) => true
+            };
+        }
+    }
+
+    public class Offer_RenameMe : Promise
+    {
+        public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
+        {
+            return (offeror is CoastalSteel || offeror is Algernon)
+                   && (offeree is CoastalSteel || offeree is Algernon);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as Offer_RenameMe;
+            if (o == null)
+                return false;
+            return true;
+        }
+    }
+
+    public class Acceptanct_RenameMe : Offer_RenameMe
+    {
+        public override bool Equals(object obj)
+        {
+            var o = obj as Acceptanct_RenameMe;
+            if (o == null)
+                return false;
+            return true;
+        }
+    }
+
+    public class CoastalSteel : LegalPerson
+    {
+        public CoastalSteel(): base("") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("", DBNull.Value),
+            };
+        }
+    }
+
+    public class Algernon : LegalPerson
+    {
+        public Algernon(): base("") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("", DBNull.Value),
+            };
+        }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/RemedyTests/LeevFooteTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Contracts;
+using NoFuture.Rand.Law.US.Contracts.Remedy.MoneyDmg;
+using NoFuture.Rand.Law.US.Contracts.Terms;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Tests.LawTests.ContractTests.RemedyTests
+{
+    /// <summary>
+    /// LEE v. FOOTE District of Columbia Court of Appeals 481 A.2d 484 (D.C. 1984)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// 
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class LeevFooteTests
+    {
+        [Test]
+        public void LeevFoote()
+        {
+            var testContract = new ComLawContract<Promise>
+            {
+                Offer = new OfferCarpentry(),
+                Acceptance = o => o is OfferCarpentry ? new AcceptanctDoPlumingWork() : null,
+                Assent = new MutualAssent
+                {
+                    IsApprovalExpressed = lp => true,
+                    TermsOfAgreement = lp =>
+                    {
+                        switch (lp)
+                        {
+                            case Lee _:
+                                return ((Lee)lp).GetTerms();
+                            case Foote _:
+                                return ((Foote)lp).GetTerms();
+                            default:
+                                return null;
+                        }
+                    }
+                }
+            };
+
+            testContract.Consideration = new Consideration<Promise>(testContract)
+            {
+                IsGivenByPromisee = (lp, p) => true,
+                IsSoughtByPromisor = (lp, p) => true
+            };
+
+            var testResult = testContract.IsValid(new Lee(), new Foote());
+            Assert.IsTrue(testResult);
+
+            var testSubject = new QuantumMeruitRestitution<Promise>(testContract)
+            {
+                CalcPerformanceRenderedValue = lp => lp is Lee ? 1m : 0m,
+            };
+
+            testResult = testSubject.IsValid(new Lee(), new Foote());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class OfferCarpentry : Promise
+    {
+        public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
+        {
+            return (offeror is Lee || offeror is Foote)
+                   && (offeree is Lee || offeree is Foote);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as OfferCarpentry;
+            if (o == null)
+                return false;
+            return true;
+        }
+    }
+
+    public class AcceptanctDoPlumingWork : OfferCarpentry
+    {
+        public override bool Equals(object obj)
+        {
+            var o = obj as AcceptanctDoPlumingWork;
+            if (o == null)
+                return false;
+            return true;
+        }
+    }
+
+    public class Lee : LegalPerson
+    {
+        public Lee(): base("") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("", DBNull.Value),
+            };
+        }
+    }
+
+    public class Foote : LegalPerson
+    {
+        public Foote(): base("") { }
+        public ISet<Term<object>> GetTerms()
+        {
+            return new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("", DBNull.Value),
+            };
+        }
+    }
+}
