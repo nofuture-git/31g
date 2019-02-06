0d3164b51736c032fe2a2d1eaf1f357a55616e82
NoFuture <brian.starrett@gmail.com>
2019-02-06 10:58:13 -0600

    added new tests folder in Nf.Rand.Test for the next Law subject

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/CrimeTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using NoFuture.Rand.Law.US.Criminal;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Tests.LawTests.CriminalTests
+{
+    [TestFixture]
+    public class CrimeTests
+    {
+
+        [Test]
+        public void TestCompareTo()
+        {
+            ICrime[] testSubjects =
+            {
+                new Misdemeanor(), new Infraction(),
+                new Misdemeanor(), new Felony(),
+                new Infraction(), new Felony(),
+                new Infraction(), new Misdemeanor(),
+            };
+
+            Array.Sort(testSubjects);
+
+            foreach(var c in testSubjects)
+                Console.WriteLine(c.GetType().Name);
+            Assert.IsTrue(testSubjects.First() is Infraction);
+            Assert.IsTrue(testSubjects.Last() is Felony);
+        }
+    }
+}
