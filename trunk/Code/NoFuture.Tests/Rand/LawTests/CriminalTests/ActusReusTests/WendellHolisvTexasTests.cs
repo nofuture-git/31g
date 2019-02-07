dd98aca37510e129f6b2094811905788cd60a65d
NoFuture <brian.starrett@gmail.com>
2019-02-07 14:41:56 -0600

    implemented unit test on mens rea in Nf.Rand.Law

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/ActusReusTests/WendellHolisvTexasTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Criminal;
+using NoFuture.Rand.Law.US.Criminal.Elements;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Tests.LawTests.CriminalTests.ActusReusTests
+{
+    /// <summary>
+    /// 998 S.W.2d 363 (1999) Wendell Hollis OLER, Appellant, v. The STATE of Texas, Appellee. No. 05-97-01229-CR. Court of Appeals of Texas, Dallas.
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, possession is another kind of actus reus 
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class WendellHolisvTexasTests
+    {
+        [Test]
+        public void WendllHolisvTexas()
+        {
+            var testSubject = new Felony()
+            {
+                IsChargedWith = lp =>
+                {
+                    var isPossessionOfCtrlSubstance = lp is WendellHollis;
+                    var isByObtainByMisrepresentation = lp is WendellHollis;
+                    return isPossessionOfCtrlSubstance && isByObtainByMisrepresentation;
+                }
+            };
+            //court contents the actus reus is by possession, therefore omission is not applicable
+            var testPossession = new Possession();
+            testSubject.ActusReus = testPossession;
+            testPossession.IsKnowinglyProcured = lp => lp is WendellHollis;
+            var testResult = testPossession.IsValid(new WendellHollis());
+            Console.WriteLine(testPossession.ToString());
+            Assert.IsTrue(testResult);
+
+            //defendant contents no statute duty to report multiple prescriptions
+            var testOmission = new DutyToAct {IsStatuteOrigin = lp => false};
+            testResult = testOmission.IsValid(new WendellHollis());
+            Assert.IsFalse(testResult);
+        }
+    }
+
+    public class WendellHollis : LegalPerson
+    {
+        public WendellHollis() : base("WENDELL HOLLIS") { }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/MensReaTests/HaroldStaplesvUsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NoFuture.Rand.Law;
+using NoFuture.Rand.Law.US.Criminal;
+using NoFuture.Rand.Law.US.Criminal.Elements;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Tests.LawTests.CriminalTests.MensReaTests
+{
+    /// <summary>
+    /// HAROLD E. STAPLES, III, PETITIONER v. UNITED STATES No. 92-1441
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// 
+    /// ]]>
+    /// </remarks>
+    [TestFixture()]
+    public class HaroldStaplesvUsTests
+    {
+        [Test]
+        public void HaroldStaplesvUs()
+        {
+            var testSubject = new Felony
+            {
+                IsChargedWith = lp =>
+                {
+                    var hs = lp as HaroldStaples;
+                    return (hs?.IsPossessionOfAr15Rifle ?? false) && (hs.IsAr15RifleFullyAutoFire);
+                }
+            };
+
+            testSubject.ActusReus.IsAction = lp => lp is HaroldStaples;
+            testSubject.ActusReus.IsVoluntary = lp => lp is HaroldStaples;
+
+            //mens rea is not needed for Fed Statute intended to regulate
+            //for example ownership of hand gernades was a crime without mens rea
+            testSubject.MensRea = null;
+            var testResult = testSubject.IsValid(new HaroldStaples());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+
+            //for this case, gun ownership is not the same thing
+            testSubject.MensRea = new MensRea();
+            testResult = testSubject.IsValid(new HaroldStaples());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsFalse(testResult);
+        }
+    }
+
+    public class HaroldStaples : LegalPerson
+    {
+        public HaroldStaples(): base("HAROLD E. STAPLES") { }
+        public bool IsPossessionOfAr15Rifle => true;
+        public bool IsAr15RifleFullyAutoFire => true;
+    }
+}
