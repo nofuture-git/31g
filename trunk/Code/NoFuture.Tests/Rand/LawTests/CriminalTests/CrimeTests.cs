b1b91b62c336633a98c8731647fd4a2624940f60
NoFuture <brian.starrett@gmail.com>
2019-02-12 14:30:59 -0600

    added explicit terms for the legal concepts of deadly and nondeadly force

--- trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/CrimeTests.cs
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/CrimeTests.cs
@@ -43,3 +43,68 @@ namespace NoFuture.Rand.Law.Tests.CriminalTests
         public TestPerson00() : base("TEST PERSON")  { }
     }
 }
+
+namespace We.Whatever
+{
+    public class Classic00
+    {
+        public virtual object Example00(object something) { return new object(); }
+        public virtual bool Example01(object something) { return false; }
+        public virtual void Example02(object something) { }
+
+        public virtual object Composition(object something)
+        {
+            var o = Example00(something);
+            if(Example01(o))
+                Example02(o);
+            return o;
+        }
+    }
+
+    public class ByEnclosures00
+    {
+        public virtual Func<object, object> Example00 { get; set; } = o => new object();
+        public virtual Predicate<object> Example01 { get; set; } = o => false;
+        public virtual Action<object> Example02 { get; set; } = o => { };
+
+        public virtual Func<object, ByEnclosures00, object> Composition { get; set; } = (something, myself) =>
+        {
+            var o = myself.Example00(something);
+            if (myself.Example01(o))
+                myself.Example02(o);
+            return o;
+        };
+    }
+
+}
+
+namespace We.Whatever.Tests
+{
+    public class TestOfClassic00 : Classic00
+    {
+        public override bool Example01(object something)
+        {
+            return true;
+        }
+    }
+
+    [TestFixture]
+    public class TestClassicvByEnclousure
+    {
+        [Test]
+        public void TestExamples()
+        {
+            var enclosure = new ByEnclosures00
+            {
+                Example00 = o => new Classic00(),
+                Example01 = o => true,
+                Example02 = o => {}
+            };
+
+
+        }
+    }
+
+}
+
+
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/TermsTests/ForceTests.cs
@@ -0,0 +1,18 @@
+﻿using NoFuture.Rand.Law.US.Criminal.Terms;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Tests.CriminalTests.TermsTests
+{
+    [TestFixture]
+    public class ForceTests
+    {
+        [Test]
+        public void TestGetCategoryRank()
+        {
+            var testSubject00 = new DeadlyForce();
+            var testSubject01 = new NondeadlyForce();
+
+            Assert.IsTrue(testSubject00.GetCategoryRank() > testSubject01.GetCategoryRank());
+        }
+    }
+}
