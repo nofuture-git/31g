9e66fc0b6e6a52db7c77679d826cef93ee6c7361
NoFuture <brian.starrett@gmail.com>
2019-02-13 17:23:16 -0600

    added notes from text to various declaration annotations

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/DefenseTests/DefenseOfOtherTests/CommonwealthvMirandaTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace NoFuture.Rand.Law.Tests.CriminalTests.DefenseTests.DefenseOfOtherTests
+{
+    /// <summary>
+    /// COMMONWEALTH v. Maria A. MIRANDA. No. 08-P-2094. Decided: June 21, 2010
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// 
+    /// ]]>
+    /// </remarks>
+    [TestFixture()]
+    public class CommonwealthvMirandaTests
+    {
+        [Test]
+        public void CommonwealthvMiranda()
+        {
+
+        }
+    }
+
+    public class Miranda : LegalPerson
+    {
+        public Miranda() : base("MARIA A. MIRANDA") {}
+    }
+
+    public class DemetriaBattle : LegalPerson { }
+
+}
