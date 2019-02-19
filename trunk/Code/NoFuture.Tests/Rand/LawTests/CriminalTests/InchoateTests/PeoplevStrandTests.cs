89353acaaee94d356eb4fa13636926427ea26dc8
NoFuture <brian.starrett@gmail.com>
2019-02-19 17:39:51 -0600

    added another unit test for Inchoate

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/InchoateTests/PeoplevStrandTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.Inchoate.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.InchoateTests
+{
+    /// <summary>
+    /// People v. Strand, 539 N.W.2d 739 (1995)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, one does not commit an assault intending to attempt to commit a crime
+    /// ]]>
+    /// </remarks>
+    [TestFixture()]
+    public class PeoplevStrandTests
+    {
+        [Test]
+        public void PeoplevStrand()
+        {
+            var testCrime = new Felony
+            {
+                MensRea = new GeneralIntent
+                {
+                    IsIntentOnWrongdoing = lp => lp is Strand
+                },
+                ActusReus = new Attempt
+                {
+                }
+            };
+
+            var testResult = testCrime.IsValid(new Strand());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsFalse(testResult);
+        }
+    }
+
+    public class Strand : LegalPerson
+    {
+        
+    }
+}
