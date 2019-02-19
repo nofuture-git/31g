925377f241f062d26c4b4619474572a3ce72bcb5
NoFuture <brian.starrett@gmail.com>
2019-02-19 14:24:57 -0600

    extracted Nf.Rand.Law.Contract and Criminal into their own assemblies with there own unit test assemblies likewise

--- trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/ContractTermTests.cs
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ContractTests/ContractTermTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NoFuture.Rand.Law;
 using NoFuture.Rand.Law.US.Contracts.Terms;
 using NUnit.Framework;
@@ -64,6 +66,104 @@ namespace NoFuture.Rand.Law.Tests.ContractTests
 
             testResult = testSubject01.CompareTo(testSubject00);
             Assert.IsTrue(testResult > 0);
+        }
+
+        [Test]
+        public void TestDecorator()
+        {
+            var testSubject = new Term<object>("test term", DBNull.Value);
+            testSubject.As(new OralTerm()).As(new TechnicalTerm());
+
+            var testResultName = testSubject.GetCategory();
+            Console.WriteLine(testResultName);
+
+            var testResult = testSubject.IsCategory(new OralTerm());
+            Assert.IsTrue(testResult);
+
+            testResult = testSubject.IsCategory(typeof(TechnicalTerm));
+            Assert.IsTrue(testResult);
+
+            testResult = testSubject.IsCategory(typeof(CommonUseTerm));
+            Assert.IsFalse(testResult);
+        }
+
+        [Test]
+        public void TestGetAdditionalTerms()
+        {
+            var terms00 = new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("sell ranch", 0, new WrittenTerm()),
+                new ContractTerm<object>("repurchase option", 1, new OralTerm())
+            };
+            var terms01 = new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("sell ranch", 0, new WrittenTerm()),
+            };
+
+            var testResult = Term<object>.GetAdditionalTerms(terms00, terms01);
+
+            Assert.IsNotNull(testResult);
+            Assert.IsTrue(testResult.Any());
+
+        }
+
+        [Test]
+        public void TestGetInNameAgreedTerms()
+        {
+            var terms00 = new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("sell ranch", 0, new WrittenTerm()),
+                new ContractTerm<object>("repurchase option", 1, new OralTerm())
+            };
+            var terms01 = new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("sell ranch", 0, new WrittenTerm()),
+            };
+
+            var testResult = Term<object>.GetInNameAgreedTerms(terms00, terms01);
+
+            Assert.IsNotNull(testResult);
+            Assert.IsTrue(testResult.Any());
+            Assert.IsTrue(testResult.Count == 1);
+            Assert.AreEqual("sell ranch", testResult.First().Name);
+        }
+
+        [Test]
+        public void TestGetAgreedTerms()
+        {
+            var terms00 = new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("sell ranch", 0, new WrittenTerm()),
+                new ContractTerm<object>("repurchase option", 1, new OralTerm()),
+                new ContractTerm<object>("wrong value", 12, new OralTerm()),
+            };
+            var terms01 = new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("sell ranch", 0, new WrittenTerm()),
+                new ContractTerm<object>("wrong name", 1),
+                new ContractTerm<object>("wrong value", 11, new OralTerm()),
+            };
+
+            var testResult = Term<object>.GetAgreedTerms(terms00, terms01);
+
+            Assert.IsNotNull(testResult);
+            Assert.IsTrue(testResult.Any());
+            Assert.IsTrue(testResult.Count == 1);
+            Assert.AreEqual("sell ranch", testResult.First().Name);
+        }
+
+        [Test]
+        public void TestThrowOnDupRefersTo()
+        {
+            var terms01 = new HashSet<Term<object>>
+            {
+                new ContractTerm<object>("first name", 0, new WrittenTerm()),
+                new ContractTerm<object>("second name", 0),
+                new ContractTerm<object>("term 00", 11, new OralTerm()),
+                new ContractTerm<object>("term 01", 12, new OralTerm()),
+                new ContractTerm<object>("term 02", 13, new OralTerm()),
+            };
+            Assert.Throws<AggregateException>(() => Term<object>.ThrowOnDupRefersTo(terms01));
         }
     }
 }
