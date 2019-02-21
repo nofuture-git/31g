46b80f98e7bdd7bcbb02c5f2d634621deeb08644
NoFuture <brian.starrett@gmail.com>
2019-02-21 10:10:38 -0600

    added the last actus reus to Inchoate assembly and unit tests thereof

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/InchoateTests/ExampleSolicitationTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFuture.Rand.Law.Criminal.Inchoate.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.InchoateTests
+{
+    [TestFixture()]
+    public class ExampleSolicitationTests
+    {
+        [Test]
+        public void ExampleSolicitation()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Solicitation
+                {
+                    IsInduceAnotherToCrime = lp => lp is JimmyRequestorEg
+                },
+                MensRea = new SpecificIntent
+                {
+                    IsKnowledgeOfWrongdoing = lp => lp is JimmyRequestorEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new JimmyRequestorEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class JimmyRequestorEg : LegalPerson
+    {
+        public JimmyRequestorEg() : base("JIMMY REQUESTOR") {}
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/InchoateTests/PlantervStateTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.Inchoate.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.InchoateTests
+{
+    /// <summary>
+    /// Planter v. State, 9 S.W. 3d 156 (1999)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, with solicitation, which is mostly what is said, understanding the lang is critical
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class PlantervStateTests
+    {
+        [Test]
+        public void PlantervState()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Solicitation
+                {
+                    IsInduceAnotherToCrime = lp =>
+                    {
+                        //defendant want to get paid to make the hit, not pay for it
+                        var wasSolicited = Planter.GetSolicitation(lp);
+                        return string.Equals(wasSolicited, "you do it");
+                    }
+                },
+                MensRea = new SpecificIntent
+                {
+                    IsKnowledgeOfWrongdoing = lp => lp is Planter
+                }
+            };
+            var testResult = testCrime.IsValid(new Planter());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsFalse(testResult);
+        }
+    }
+
+    public class Planter : LegalPerson
+    {
+        public Planter(): base("WILLIAM EDWARD PLANTER") { }
+
+        public static string GetSolicitation(ILegalPerson lp)
+        {
+            if (lp is Planter)
+                return "I'll do it";
+            if (lp is Government)
+                return "you do it";
+            return null;
+        }
+    }
+}
