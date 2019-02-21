4b1ed0405e637851df90bbe2c46caa4bcb5ba034
NoFuture <brian.starrett@gmail.com>
2019-02-21 14:12:42 -0600

    added new type in Nf.Rand.Law.Criminal.Homicide and unit test thereof

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HomicideTests/ExampleFirstDegreeTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFuture.Rand.Law.Criminal.Homicide.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.HomicideTests
+{
+    [TestFixture]
+    public class ExampleFirstDegreeTests
+    {
+        [Test]
+        public void ExampleFirstDegree()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new MurderFirstDegree
+                {
+                    IsPremediated = JoanniePremedEg.IsPremeditated,
+                    IsCorpusDelicti = lp => lp is JoanniePremedEg
+                },
+                MensRea = new DeadlyWeapon("handgun")
+                {
+                    IsUtilized = lp => lp is JoanniePremedEg,
+                    IsCanCauseDeath = lp => lp is JoanniePremedEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new JoanniePremedEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class JoanniePremedEg : LegalPerson
+    {
+        public JoanniePremedEg() : base("JOANNIE PREMED") { }
+
+        public bool IsCalm { get; set; } = true;
+        public bool IsMethodical { get; set; } = true;
+        public bool IsManufacturedExcuse { get; set; } = true;
+        public bool IsFakingActions { get; set; } = true;
+        public bool IsMutlipleShots { get; set; } = true;
+
+        public bool IsDeliberate => IsCalm && IsMethodical;
+        public bool IsPlanned => IsManufacturedExcuse && IsFakingActions;
+        public bool IsSpecificIntent => IsMutlipleShots;
+
+        public static bool IsPremeditated(ILegalPerson lp)
+        {
+            var joannie = lp as JoanniePremedEg;
+            if (joannie == null)
+                return false;
+            return joannie.IsDeliberate || joannie.IsPlanned || joannie.IsSpecificIntent;
+        }
+    }
+}
