8cc0cfddb8938016326bbc6a11d23dc5f182058a
NoFuture <brian.starrett@gmail.com>
2019-02-22 17:05:06 -0600

    added the Incest type to Criminal/HominiLupus/Rand.Law.Criminal.HominiLupus.csproj and unit tests thereof

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HominiLupusTests/ExampleIncestTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.HominiLupusTests
+{
+    [TestFixture]
+    public class ExampleIncestTests
+    {
+        [Test]
+        public void ExampleIncest()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Incest
+                {
+                    GetVictim = lps => lps.FirstOrDefault(lp => lp.Name == "HARRIET INCEST"),
+                    IsFamilyRelation = (lp1, lp2) => (lp1 is HalIncestEg || lp1 is HarrietIncestEg)
+                                                     && (lp2 is HalIncestEg || lp2 is HarrietIncestEg)
+                },
+                MensRea = new GeneralIntent
+                {
+                    IsKnowledgeOfWrongdoing = lp => lp is HalIncestEg || lp is HarrietIncestEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new HalIncestEg(), new HarrietIncestEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+
+        [Test]
+        public void TestIntentIsNull()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Incest
+                {
+                    GetVictim = lps => lps.FirstOrDefault(lp => lp.Name == "HARRIET INCEST"),
+                    IsFamilyRelation = (lp1, lp2) => (lp1 is HalIncestEg || lp1 is HarrietIncestEg)
+                                                     && (lp2 is HalIncestEg || lp2 is HarrietIncestEg)
+                },
+                MensRea = null
+            };
+
+            var testResult = testCrime.IsValid(new HalIncestEg(), new HarrietIncestEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsFalse(testResult);
+        }
+    }
+
+    public class HalIncestEg : LegalPerson
+    {
+        public HalIncestEg() : base("HAL INCEST") { }
+    }
+
+    public class HarrietIncestEg : LegalPerson
+    {
+        public HarrietIncestEg() : base("HARRIET INCEST") { }
+    }
+}
