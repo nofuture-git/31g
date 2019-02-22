77205a4c5698fd450f815d499558e902a520e137
NoFuture <brian.starrett@gmail.com>
2019-02-22 15:07:39 -0600

    expanded the criminal law concept of Consent which is like with additional unit tests

--- trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HominiLupusTests/ExampleRapeTests.cs
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HominiLupusTests/ExampleRapeTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using NoFuture.Rand.Law.Criminal.HominiLupus.US.Elements;
 using NoFuture.Rand.Law.Criminal.US;
 using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
@@ -29,6 +30,83 @@ namespace NoFuture.Rand.Law.Criminal.Tests.HominiLupusTests
             Console.WriteLine(testCrime.ToString());
             Assert.IsTrue(testResult);
         }
+
+        [Test]
+        public void TestLackOfConsent()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Rape
+                {
+                    IsSexualIntercourse = lp => lp is AlexGamerEg,
+                    Consent = new Consent
+                    {
+                        IsFirmDenial = lp => lp is BrandySisterEg,
+                        GetVictim = lps => lps.FirstOrDefault(lp => lp.Name == "BRANDY SISTER")
+                    }
+                },
+                MensRea = new Purposely
+                {
+                    IsIntentOnWrongdoing = lp => lp is AlexGamerEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new AlexGamerEg(), new BrandySisterEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+
+        [Test]
+        public void TestStatutoryRape()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Rape
+                {
+                    IsSexualIntercourse = lp => lp is AlexGamerEg,
+                    Consent = new Consent
+                    {
+                        IsCapableThereof = lp => ((lp as BrandySisterEg)?.Age  ?? 18) >= 16,
+                        GetVictim = lps => lps.FirstOrDefault(lp => lp.Name == "BRANDY SISTER")
+                    }
+                },
+                MensRea = new Purposely
+                {
+                    IsIntentOnWrongdoing = lp => lp is AlexGamerEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new AlexGamerEg(), new BrandySisterEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+
+        [Test]
+        public void TestConsentDefense()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Rape
+                {
+                    IsSexualIntercourse = lp => lp is AlexGamerEg,
+                    Consent = new Consent
+                    {
+                        //assented 
+                        IsFirmDenial = lp => false,
+                        IsCapableThereof = lp => ((lp as BrandySisterEg)?.Age ?? 18) >= 16,
+                        GetVictim = lps => lps.FirstOrDefault(lp => lp.Name == "BRANDY SISTER")
+                    }
+                },
+                MensRea = new Purposely
+                {
+                    IsIntentOnWrongdoing = lp => lp is AlexGamerEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new AlexGamerEg(), new BrandySisterEg(){Age = 21});
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsFalse(testResult);
+        }
     }
 
     public class AlexGamerEg : LegalPerson
@@ -44,5 +122,7 @@ namespace NoFuture.Rand.Law.Criminal.Tests.HominiLupusTests
     public class BrandySisterEg : LegalPerson
     {
         public BrandySisterEg() : base("BRANDY SISTER") { }
+
+        public int Age { get; set; } = 14;
     }
 }
