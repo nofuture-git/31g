a2cd34da8e69cfa23640c97628f356183b191fed
NoFuture <brian.starrett@gmail.com>
2019-02-22 13:36:46 -0600

    added new assembly for sex offenses, assault, kidnapping, etc of Criminal Law

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HominiLupusTests/ExampleRapeTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.HominiLupusTests
+{
+    [TestFixture]
+    public class ExampleRapeTests
+    {
+        [Test]
+        public void ExampleRape()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Rape
+                {
+                    IsByThreatOfForce = lp => lp is AlexGamerEg,
+                    IsSexualIntercourse = lp => lp is AlexGamerEg
+                },
+                MensRea = new Purposely
+                {
+                    IsIntentOnWrongdoing = lp => lp is AlexGamerEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new AlexGamerEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class AlexGamerEg : LegalPerson
+    {
+        public AlexGamerEg() : base("ALEX GAMER") { }
+    }
+
+    public class BradAlsogamerEg : LegalPerson
+    {
+        public BradAlsogamerEg(): base("BRAD ALSOGAMER") { }
+    }
+
+    public class BrandySisterEg : LegalPerson
+    {
+        public BrandySisterEg() : base("BRANDY SISTER") { }
+    }
+}
