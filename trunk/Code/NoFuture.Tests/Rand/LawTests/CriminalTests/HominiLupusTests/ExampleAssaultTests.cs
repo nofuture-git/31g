411e9d119279e9d611b01d8007d91739f51a3881
NoFuture <brian.starrett@gmail.com>
2019-02-25 16:14:28 -0600

    more refactor to have various interfaces represent base ideas of criminal law while implementors are compositions thereof

--- trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HominiLupusTests/ExampleAssaultTests.cs
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HominiLupusTests/ExampleAssaultTests.cs
@@ -2,6 +2,7 @@
 using NoFuture.Rand.Law.Criminal.HominiLupus.US.Elements;
 using NoFuture.Rand.Law.Criminal.US;
 using NoFuture.Rand.Law.Criminal.US.Elements.Intent;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
 using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
 using NUnit.Framework;
 
@@ -47,6 +48,44 @@ namespace NoFuture.Rand.Law.Criminal.Tests.HominiLupusTests
             Console.WriteLine(testCrime.ToString());
             Assert.IsFalse(testResult);
         }
+
+        [Test]
+        public void TestThreatenedBatteryAssault()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new ThreatenedBatteryAssault
+                {
+                    IsByThreatOfForce = lp => ((lp as DianaPistolEg)?.IsAimPistol ?? false)
+                                              && ((DianaPistolEg)lp).IsSheCockGunHammer,
+                    IsPresentAbility = lp => lp is DianaPistolEg
+                },
+                MensRea = new DeadlyWeapon("pistol", new Purposely())
+            };
+
+            var testResult = testCrime.IsValid(new DianaPistolEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+
+        [Test]
+        public void TestThreatenedBatteryAssault_GeneralIntent()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new ThreatenedBatteryAssault
+                {
+                    IsByThreatOfForce = lp => ((lp as DianaPistolEg)?.IsAimPistol ?? false)
+                                              && ((DianaPistolEg)lp).IsSheCockGunHammer,
+                    IsPresentAbility = lp => lp is DianaPistolEg
+                },
+                MensRea = new DeadlyWeapon("pistol", new GeneralIntent())
+            };
+
+            var testResult = testCrime.IsValid(new DianaPistolEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsFalse(testResult);
+        }
     }
 
     public class DianaPistolEg : LegalPerson
@@ -55,5 +94,6 @@ namespace NoFuture.Rand.Law.Criminal.Tests.HominiLupusTests
         public bool IsAimPistol { get; set; } = true;
         public bool IsPullTrigger { get; set; } = true;
         public bool IsPistolLoaded { get; set; } = true;
+        public bool IsSheCockGunHammer { get; set; } = true;
     }
 }
