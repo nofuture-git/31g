0813f66f0067f7d943f46cb1795befb72a0bcaaf
NoFuture <brian.starrett@gmail.com>
2019-02-26 12:01:06 -0600

    added some unit tests for Stalking

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HominiLupusTests/BurkevStateTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US.Credible;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.HominiLupusTests
+{
+    [TestFixture()]
+    public class BurkevStateTests
+    {
+        [Test]
+        public void BurkevState()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Stalking
+                {
+                    IsApparentAbility = lp => lp is Burke,
+                    Occasions = new IAgitate[]
+                    {
+                        new Harass
+                        {
+                            IsSubstantialEmotionalDistress = lp => lp is Burke
+                        },
+                        new DeclareLove
+                        {
+                            IsCauseToFearSafety = lp => lp is Burke
+                        }
+                    }
+                },
+                MensRea = new Purposely
+                {
+                    IsIntentOnWrongdoing = lp => lp is Burke
+                }
+            };
+
+            var testResult = testCrime.IsValid(new Burke());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsFalse(testResult);
+        }
+    }
+
+    public class Burke : LegalPerson
+    {
+        public Burke() : base("SEAN M. BURKE") { }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HominiLupusTests/StatevHolbachTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US.Credible;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.HominiLupusTests
+{
+    /// <summary>
+    /// State v. Holbach, 2009 ND 37 (2009)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, the agitation must induce a real fear
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class StatevHolbachTests
+    {
+        [Test]
+        public void StatevHolbach()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Stalking
+                {
+                    IsApparentAbility = lp => lp is Holbach,
+                    Occasions = new IAgitate[]
+                    {
+                        new ThreateningWords
+                        {
+                            IsCauseToFearSafety = lp => lp is Holbach,
+                        }, 
+                        new ThreateningWords
+                        {
+                            IsCauseToFearSafety = lp => lp is Holbach
+                        }
+                    }
+                },
+                MensRea = new Purposely
+                {
+                    IsKnowledgeOfWrongdoing = lp => lp is Holbach
+                }
+            };
+
+            var testResult = testCrime.IsValid(new Holbach());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class Holbach : LegalPerson
+    {
+        public Holbach() : base("SCOTT THOMAS BOYLE") { }
+    }
+}
