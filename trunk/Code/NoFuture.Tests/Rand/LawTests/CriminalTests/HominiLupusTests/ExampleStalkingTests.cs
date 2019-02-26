3134cf1281f2ceee5defe1d4b60a4e973b5db212
NoFuture <brian.starrett@gmail.com>
2019-02-26 11:25:47 -0600

    added implementations of IAgitate and then coupled them to the actus reus of Stalking along with unit tests thereof

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HominiLupusTests/ExampleStalkingTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US.Credible;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.HominiLupusTests
+{
+    [TestFixture]
+    public class ExampleStalkingTests
+    {
+        [Test]
+        public void ExampleNotStalking()
+        {
+            var testCrime = new Misdemeanor
+            {
+                ActusReus = new Stalking
+                {
+                    Occasions = new[] {new DeclareLove(), new DeclareLove(),},
+                    IsApparentAbility = lp => lp is ElliotStalkerEg
+                },
+                MensRea = new GeneralIntent
+                {
+                    IsKnowledgeOfWrongdoing = lp => lp is ElliotStalkerEg
+                }
+            };
+            var testResult = testCrime.IsValid(new ElliotStalkerEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsFalse(testResult);
+        }
+
+        [Test]
+        public void ExampleIsStalking()
+        {
+            var testCrime = new Misdemeanor
+            {
+                ActusReus = new Stalking
+                {
+                    Occasions = new IAgitate[]
+                    {
+                        new Pursue
+                        {
+                            IsCauseToFearSafety = lp => lp is ElliotStalkerEg
+                        },
+                        new Approach
+                        {
+                            IsCauseToFearSafety = lp => lp is ElliotStalkerEg
+                        },
+                        new ThreateningWords("make her pay")
+                        {
+                            IsCauseToFearSafety = lp => lp is ElliotStalkerEg
+                        },
+                        new ThreateningGesture("carving death threat on door")
+                        {
+                            IsSubstantialEmotionalDistress = lp => lp is ElliotStalkerEg
+                        }, 
+                    },
+                    IsApparentAbility = lp => lp is ElliotStalkerEg
+                },
+                MensRea = new GeneralIntent
+                {
+                    IsKnowledgeOfWrongdoing = lp => lp is ElliotStalkerEg
+                }
+            };
+            var testResult = testCrime.IsValid(new ElliotStalkerEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class DeclareLove : CredibleBase
+    {
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            AddReasonEntry("declaring love isn't illegal nor substantial - just annoying");
+            return false;
+        }
+    }
+
+    public class ElliotStalkerEg : LegalPerson
+    {
+        public ElliotStalkerEg() : base("ELLIOT STALKER") { }
+    }
+}
