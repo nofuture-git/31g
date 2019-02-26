4a0259d42876b2392a7e0d3776efa1620946cee8
NoFuture <brian.starrett@gmail.com>
2019-02-26 17:10:58 -0600

    added next major unit of Rand.Law.Criminal AgainstProperty along with implementations based on Model Penal Code since text kinda falls apart on predicate logic

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/TheftTests/ExampleByTakingTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements.Theft;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.TheftTests
+{
+    [TestFixture]
+    public class ExampleByTakingTests
+    {
+        [Test]
+        public void ExampleFiveFingerTheft()
+        {
+            var testCrime = new Misdemeanor
+            {
+                ActusReus = new ByTaking
+                {
+                    AmountOfTheft = 1.25m,
+                    SubjectOfTheft = new ChewingGum(),
+                    IsTakenUnlawful = lp => lp is JeremyTheifEg,
+                    IsToBenefitUnentitled = lp => lp is JeremyTheifEg
+                },
+                MensRea = new Purposely
+                {
+                    IsKnowledgeOfWrongdoing = lp => lp is JeremyTheifEg
+                }
+            };
+            var testResult = testCrime.IsValid(new JeremyTheifEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class ChewingGum : LegalProperty
+    {
+
+    }
+
+    public class JeremyTheifEg : LegalPerson
+    {
+        public JeremyTheifEg() : base("JEREMY THEIF") {}
+    }
+}
