fffc97c4ad5926efe95bfc0380e0408760f014a7
NoFuture <brian.starrett@gmail.com>
2019-02-26 13:33:04 -0600

    added the FalseImprisonment type and changed it to have Kidnapping extend it, added unit tests thereof

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HominiLupusTests/ExampleFalseImprisonmentTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.HominiLupusTests
+{
+    [TestFixture]
+    public class ExampleFalseImprisonmentTests
+    {
+        [Test]
+        public void ExampleIsFalseImprisonment()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new FalseImprisonment
+                {
+                    IsConfineVictim = lp => lp is ThomasHitchhikerEg
+                },
+                MensRea = new GeneralIntent
+                {
+                    IsKnowledgeOfWrongdoing = lp => lp is ThomasHitchhikerEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new ThomasHitchhikerEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/HominiLupusTests/ExampleKidnappingTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using NoFuture.Rand.Law.Criminal.HominiLupus.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.HominiLupusTests
+{
+    [TestFixture]
+    public class ExampleKidnappingTests
+    {
+        [Test]
+        public void ExampleNotKidnapping()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Kidnapping
+                {
+                    IsConfineVictim = lp => lp is JosephAbbyraperEg
+                },
+                MensRea = new Purposely
+                {
+                    IsIntentOnWrongdoing = lp => lp is JosephAbbyraperEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new JosephAbbyraperEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsFalse(testResult);
+        }
+
+        [Test]
+        public void ExampleKidnappingWrongIntent()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Kidnapping
+                {
+                    IsConfineVictim = lp => lp is JosephAbbyraperEg,
+                    IsAsportation = lp => lp is JosephAbbyraperEg
+                },
+                MensRea = new GeneralIntent
+                {
+                    IsIntentOnWrongdoing = lp => lp is JosephAbbyraperEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new JosephAbbyraperEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsFalse(testResult);
+        }
+
+        [Test]
+        public void ExampleIsKidnapping()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Kidnapping
+                {
+                    IsConfineVictim = lp => lp is JosephAbbyraperEg,
+                    IsAsportation = lp => lp is JosephAbbyraperEg
+                },
+                MensRea = new Purposely
+                {
+                    IsIntentOnWrongdoing = lp => lp is JosephAbbyraperEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new JosephAbbyraperEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+
+        [Test]
+        public void ExampleKidnappingWithConsent()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Kidnapping
+                {
+                    IsConfineVictim = lp => lp is ThomasHitchhikerEg,
+                    IsAsportation = lp => lp is ThomasHitchhikerEg,
+                    Consent = new Consent
+                    {
+                        GetVictim = lps => lps.FirstOrDefault(lp => lp.Name == "SHAWNA HITCHINHIKEN"),
+                        IsCapableThereof = lp => lp is ShawnaHitchinhikenEg,
+                        IsFirmDenial = lp => false
+                    }
+                },
+                MensRea = new Purposely
+                {
+                    IsIntentOnWrongdoing = lp => false,
+                    IsKnowledgeOfWrongdoing = lp => false
+                },
+                
+            };
+
+            var testResult = testCrime.IsValid(new ThomasHitchhikerEg(), new ShawnaHitchinhikenEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsFalse(testResult);
+        }
+    }
+
+    public class JosephAbbyraperEg : LegalPerson
+    {
+        public JosephAbbyraperEg() : base("JOSEPH ABBYRAPER") { }
+    }
+
+    public class ThomasHitchhikerEg : LegalPerson
+    {
+        public ThomasHitchhikerEg() : base("THOMAS HITCHHIKER") { }
+    }
+
+    public class ShawnaHitchinhikenEg : LegalPerson
+    {
+        public ShawnaHitchinhikenEg() : base ("SHAWNA HITCHINHIKEN") { }
+    }
+}
