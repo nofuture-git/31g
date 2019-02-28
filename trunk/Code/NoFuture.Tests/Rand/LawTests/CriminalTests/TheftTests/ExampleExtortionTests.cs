959115e0113c46d4fa0d8f7b5dbfbc4abf64a282
NoFuture <brian.starrett@gmail.com>
2019-02-28 16:53:44 -0600

    added test for ByExtortion and modified its implementation based on text reading

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/TheftTests/ExampleExtortionTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements.Theft;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.TheftTests
+{
+    [TestFixture]
+    public class ExampleExtortionTests
+    {
+        [Test]
+        public void ExampleExtortionAct()
+        {
+            var testAct = new ByExtortion
+            {
+                IsTakenPossession = lp => lp is RodneyBlackmailEg,
+                Threatening = new ByExtortion.ByThreatening
+                {
+                    IsToAccuseOfCrime = lp => lp is RodneyBlackmailEg
+                },
+                SubjectOfTheft = new LegalProperty("fifteen thousand dollars"),
+                AmountOfTheft = 15000m,
+            };
+
+            var testResult = testAct.IsValid(new RodneyBlackmailEg(), new LindseyDealinEg());
+            Console.WriteLine(testAct.ToString());
+            Assert.IsTrue(testResult);
+        }
+
+        [Test]
+        public void ExampleThreatenHonestlyDue()
+        {
+            var trent = new TrentThreatenEg();
+            var thousandDollars = new LegalProperty("thousand dollars") {EntitledTo = trent};
+            var testAct = new ByExtortion
+            {
+                IsTakenPossession = lp => lp is TrentThreatenEg,
+                Threatening = new ByExtortion.ByThreatening
+                {
+                    IsToExposeHurtfulSecret = lp => lp is TrentThreatenEg
+                },
+                SubjectOfTheft = thousandDollars,
+                AmountOfTheft = 1000m,
+            };
+
+            var testResult = testAct.IsValid(new TrentThreatenEg(), new TaraLyingEg());
+            Console.WriteLine(testAct.ToString());
+            Assert.IsFalse(testResult);
+        }
+
+        [Test]
+        public void ExampleCaveBasedOnThreat()
+        {
+            var testAct = new ByExtortion
+            {
+                IsTakenPossession = lp => lp is RodneyBlackmailEg,
+                Threatening = new ByExtortion.ByThreatening
+                {
+                    IsToAccuseOfCrime = lp => lp is RodneyBlackmailEg
+                },
+                SubjectOfTheft = new LegalProperty("fifteen thousand dollars"),
+                AmountOfTheft = 15000m,
+                Consent = new Consent
+                {
+                    IsDenialExpressed = lp => false,
+                    IsCapableThereof = lp => lp is LindseyDealinEg
+                }
+            };
+
+            var testResult = testAct.IsValid(new RodneyBlackmailEg(), new LindseyDealinEg());
+            Console.WriteLine(testAct.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class RodneyBlackmailEg : LegalPerson
+    {
+        public RodneyBlackmailEg() : base("RODNEY BLACKMAIL") { }
+    }
+
+    public class LindseyDealinEg : Victim
+    {
+        public LindseyDealinEg() : base("LINDSEY DEALIN") { }
+    }
+
+    public class TrentThreatenEg : LegalPerson
+    {
+        public TrentThreatenEg() :base("TRENT THREATEN") {  }
+    }
+
+    public class TaraLyingEg : Victim
+    {
+        public TaraLyingEg() : base("TARA LYING") { }
+    }
+}
