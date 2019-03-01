4ad8eb9b005588f47b38a2751b65e89e32bd71bb
NoFuture <brian.starrett@gmail.com>
2019-03-01 17:39:40 -0600

    added the Robbery actus reus and a unit test thereof

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/TheftTests/ExampleRobberyTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements.Theft;
+using NoFuture.Rand.Law.Criminal.US.Elements;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.TheftTests
+{
+    [TestFixture()]
+    public class ExampleRobberyTests
+    {
+        [Test]
+        public void ExampleRobberyActTest()
+        {
+            var testAct = new Robbery
+            {
+                Consent = new Consent
+                {
+                    IsDenialExpressed = lp => lp is LindseyDealinEg,
+                    IsCapableThereof = lp => lp is LindseyDealinEg
+                },
+                IsTakenPossession = lp => lp is RodneyBlackmailEg,
+                IsAsportation = lp => lp is RodneyBlackmailEg,
+                IsByForce = lp => lp is RodneyBlackmailEg,
+                SubjectOfTheft = new LegalProperty("money"),
+                AmountOfTheft = 15000m
+            };
+
+            var testResult = testAct.IsValid(new RodneyBlackmailEg(), new LindseyDealinEg());
+            Console.WriteLine(testAct.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+}
