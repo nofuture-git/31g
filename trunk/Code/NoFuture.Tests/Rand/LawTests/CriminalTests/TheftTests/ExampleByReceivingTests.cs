6fd4ea4b66647401a9e48cb414f3dd0f638971ef
NoFuture <brian.starrett@gmail.com>
2019-03-01 18:22:28 -0600

    changed implementation of ByReceiving and added unit test thereof

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/TheftTests/ExampleByReceivingTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements.Theft;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.TheftTests
+{
+    [TestFixture]
+    public class ExampleByReceivingTests
+    {
+        [Test]
+        public void ExampleByReceivingActTest()
+        {
+            var testAct = new ByReceiving
+            {
+                IsPresentStolen = lp => lp is ChanelFenceEg || lp is BurtThiefEg,
+                IsTakenPossession = lp => lp is ChanelFenceEg || lp is BurtThiefEg || lp is SandraVictimEg,
+                SubjectOfTheft = new LegalProperty("designer perfume"),
+                AmountOfTheft = 5000m
+            };
+
+            var testResult = testAct.IsValid(new ChanelFenceEg(), new BurtThiefEg(), new SandraVictimEg());
+            Console.WriteLine(testAct.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class ChanelFenceEg : LegalPerson
+    {
+        public ChanelFenceEg(): base("CHANEL FENCE") {  }
+    }
+
+    public class BurtThiefEg : LegalPerson
+    {
+        public BurtThiefEg() : base("BURT THIEF") { }
+    }
+
+    public class SandraVictimEg : LegalPerson
+    {
+        public SandraVictimEg() : base("SANDRA VICTIM") {  }
+    }
+}
--- trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/TheftTests/ExampleRobberyTests.cs
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/TheftTests/ExampleRobberyTests.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements.Theft;
 using NoFuture.Rand.Law.Criminal.US.Elements;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 
 namespace NoFuture.Rand.Law.Criminal.Tests.TheftTests
 {
