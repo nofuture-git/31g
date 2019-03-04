27eafb8953e9887cdee552300e276ec281aa8d1b
NoFuture <brian.starrett@gmail.com>
2019-03-04 15:05:39 -0600

    added another type to AgainstPRoperty - CriminalMischief along with test thereof

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/PropertyDamage/ExampleCrimMischiefTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.PropertyDestruction
+{
+    [TestFixture()]
+    public class ExampleCrimMischiefTests
+    {
+        [Test]
+        public void TestCriminalMischiefAct()
+        {
+            var testAct = new CriminalMischief
+            {
+                Consent = new Consent
+                {
+                    IsDenialExpressed = lp => lp is SueBrokenstuffEg,
+                    IsCapableThereof = lp => lp is SueBrokenstuffEg
+                },
+                IsCauseOfDamage = lp => lp is JohnnyDestroyerEg,
+                IsDamaged = prop => prop?.Name == "sue's stuff",
+                SubjectProperty = new LegalProperty("sue's stuff")
+                {
+                    InPossessionOf = new SueBrokenstuffEg(),
+                    EntitledTo = new SueBrokenstuffEg()
+                }
+            };
+            var testResult = testAct.IsValid(new JohnnyDestroyerEg(), new SueBrokenstuffEg());
+            Console.WriteLine(testAct.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class JohnnyDestroyerEg : LegalPerson
+    {
+        public JohnnyDestroyerEg() : base("JOHNNY DESTROYER") { }
+    }
+
+    public class SueBrokenstuffEg : Victim
+    {
+        public SueBrokenstuffEg(): base("SUE BROKENSTUFF") {  }
+    }
+}
