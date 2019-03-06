a7898d8502affd6fc900e3cf1dd8585f28090978
NoFuture <brian.starrett@gmail.com>
2019-03-06 12:57:41 -0600

    added assembly for crimes against the government

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/AgainstGovTests/ExampleTreasonTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NoFuture.Rand.Core.Enums;
+using NoFuture.Rand.Law.Criminal.AgainstGov.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.AgainstGovTests
+{
+    [TestFixture]
+    public class ExampleTreasonTests
+    {
+        [Test]
+        public void TreasonTest()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Treason
+                {
+                    //taken oath of allegiance to Hitler
+                    IsAdheringToEnemy = lp => lp is MildredGillars,
+                    WitnessOne = new Recordings00(),
+                    WitnessTwo = new Recordings01()
+                },
+                MensRea = new Purposely
+                {
+                    IsIntentOnWrongdoing = lp => lp is MildredGillars
+                }
+            };
+
+            var testResult = testCrime.IsValid(new MildredGillars());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class Recordings00 : LegalPerson { }
+
+    public class Recordings01 : LegalPerson { }
+
+    public class MildredGillars : LegalPerson
+    {
+        public MildredGillars() : base("MILDRED GILLARS")
+        {
+            Names.Add(new Tuple<KindsOfNames, string>(KindsOfNames.Colloquial, "Axis Sally"));
+        }
+    }
+}
