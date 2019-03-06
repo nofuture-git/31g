608e7f8e7dda54a25d173e1e0cc76db77f7f4860
NoFuture <brian.starrett@gmail.com>
2019-03-06 16:31:47 -0600

    added Terrorism type to AgainstGov assembly with unit test

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/AgainstGovTests/ExampleTerrorismTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.AgainstGov.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.AgainstGovTests
+{
+    [TestFixture]
+    public class ExampleTerrorismTests
+    {
+        [Test]
+        public void TerrorismTest()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Terrorism
+                {
+                    IsByViolence = lp => lp is ZacariasMoussaoui,
+                    IsSocioPoliticalObjective = lp => lp is ZacariasMoussaoui
+                },
+                MensRea = new MaliceAforethought
+                {
+                    IsIntentOnWrongdoing = lp => lp is ZacariasMoussaoui,
+                    IsKnowledgeOfWrongdoing = lp => lp is ZacariasMoussaoui
+                }
+            };
+            var testResult = testCrime.IsValid(new ZacariasMoussaoui());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class ZacariasMoussaoui : LegalPerson
+    {
+        public ZacariasMoussaoui(): base("ZACARIAS MOUSSAOUI") { }
+    }
+}
