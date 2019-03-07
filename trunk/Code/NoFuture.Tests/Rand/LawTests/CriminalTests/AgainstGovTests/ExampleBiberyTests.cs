e3d13d1cb5cd2de6666e2312aa2947be31917a85
NoFuture <brian.starrett@gmail.com>
2019-03-07 14:07:16 -0600

    added another actus reus type to AgainstGov namespace and test thereof

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/AgainstGovTests/ExampleBiberyTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.AgainstGov.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.AgainstGovTests
+{
+    [TestFixture]
+    public class ExampleBiberyTests
+    {
+        [Test]
+        public void TestBibery()
+        {
+            var testCrime = new Felony
+            {
+                ActusReus = new Bribery
+                {
+                    IsKnowinglyProcured = lp => lp is IsabelBriberEg,
+                    IsPublicOfficial = lp => lp is IsabelBriberEg
+                },
+                MensRea = new SpecificIntent
+                {
+                    IsIntentOnWrongdoing = lp => true
+                }
+            };
+
+            var testResult = testCrime.IsValid(new IsabelBriberEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class IsabelBriberEg : LegalPerson
+    {
+        public IsabelBriberEg(): base("ISABEL BRIBER") { }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/AgainstGovTests/ExampleObstructionOfJusticeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.AgainstGov.US.Elements;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Criminal.Tests.AgainstGovTests
+{
+    [TestFixture()]
+    public class ExampleObstructionOfJusticeTests
+    {
+        [Test]
+        public void TestObstructionOfJustice()
+        {
+            var testCrime = new Misdemeanor
+            {
+                ActusReus = new ObstructionOfJustice
+                {
+                    IsRefuseToGiveEvidence = lp => lp is BarryBondsEg
+                },
+                MensRea = new Purposely
+                {
+                    IsKnowledgeOfWrongdoing = lp => lp is BarryBondsEg
+                }
+            };
+
+            var testResult = testCrime.IsValid(new BarryBondsEg());
+            Console.WriteLine(testCrime.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class BarryBondsEg : LegalPerson
+    {
+        public BarryBondsEg() : base("BARRY BONDS") {  }
+    }
+}
