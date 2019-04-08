c87a2b2f72fa0dc2a158a795113abfb9c54058e7
NoFuture <brian.starrett@gmail.com>
2019-04-08 18:14:43 -0500

    added some lines in Tokens to deal with PDF's which have all data in images

--- trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/IntelCorpvHamidiTests.cs
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/IntelCorpvHamidiTests.cs
@@ -7,6 +7,14 @@ using NoFuture.Rand.Law.US;
 
 namespace NoFuture.Rand.Law.Tort.Tests
 {
+    /// <summary>
+    /// Intel Corp. v. Hamidi, 71 P.3d 296 (Cal. 2003)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, its not trespass to personal property without damage or loss of possession
+    /// ]]>
+    /// </remarks>
     [TestFixture]
     public class IntelCorpvHamidiTests
     {
--- trunk/Code/NoFuture/Tokens/Etx.cs
+++ trunk/Code/NoFuture/Tokens/Etx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,7 @@ using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
 using Newtonsoft.Json;
 using NoFuture.Antlr.HTML;
+using Path = System.IO.Path;
 
 namespace NoFuture.Tokens
 {
@@ -32,13 +35,215 @@ namespace NoFuture.Tokens
                 for (var i = 1; i <= numOfPages; i++)
                 {
                     var pageText = PdfTextExtractor.GetTextFromPage(reader, i);
-                    if (string.IsNullOrWhiteSpace(pageText))
+                    if (String.IsNullOrWhiteSpace(pageText))
                         continue;
                     pdfText.Append(pageText);
                 }
             }
 
             return pdfText.ToString();
+        }
+
+        /// <summary>
+        /// Helper method to quick get the text out of a PDF file.
+        /// </summary>
+        public static void GetPdfText(string pdfFileFullName, int startPage = 1, int endPage = int.MaxValue, string outputDirectory = null)
+        {
+            var newFolderName = CheckPaths(pdfFileFullName, outputDirectory);
+            if (string.IsNullOrWhiteSpace(newFolderName) || !Directory.Exists(newFolderName))
+                return;
+            using (var pdf = new PdfReader(pdfFileFullName))
+            {
+                for (var pageNumber = startPage; pageNumber <= pdf.NumberOfPages; pageNumber++)
+                {
+                    if (pageNumber >= endPage)
+                        break;
+                    var pageText = PdfTextExtractor.GetTextFromPage(pdf, pageNumber);
+                    var textPage = Path.Combine(newFolderName, $"Page-{pageNumber:000}.txt");
+                    File.WriteAllText(textPage, pageText);
+                }
+            }
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/465172/merging-two-images-in-c-net
+        /// </summary>
+        public static Bitmap StackBitmaps(params string[] files)
+        {
+            //read all images into memory
+            List<Bitmap> images = new List<Bitmap>();
+            Bitmap finalImage = null;
+
+            try
+            {
+                int width = 0;
+                int height = 0;
+
+                foreach (string image in files)
+                {
+                    //create a Bitmap from the file and add it to the list
+                    var bitmap = new Bitmap(image);
+
+                    //update the size of the final bitmap
+                    width = bitmap.Width > width ? bitmap.Width : width;
+                    height += bitmap.Height+4;
+
+                    images.Add(bitmap);
+                }
+
+                //create a bitmap to hold the combined image
+                finalImage = new Bitmap(width + 8, height + 8);
+
+                //get a graphics object from the image so we can draw on it
+                using (Graphics g = Graphics.FromImage(finalImage))
+                {
+                    //set background color
+                    g.Clear(Color.White);
+
+                    //go through each image and draw it on the final image
+                    int offset = 4;
+                    foreach (Bitmap image in images)
+                    {
+                        g.DrawImage(image,
+                            new Rectangle(4, offset, image.Width, image.Height));
+                        offset += image.Height+4;
+                    }
+                }
+
+                return finalImage;
+            }
+            catch (Exception ex)
+            {
+                if (finalImage != null)
+                    finalImage.Dispose();
+
+                throw ex;
+            }
+            finally
+            {
+                //clean up memory
+                foreach (System.Drawing.Bitmap image in images)
+                {
+                    image.Dispose();
+                }
+            }
+        }
+
+        public static Bitmap PadBitmap(string imageFile, int padLen = 4)
+        {
+            //read all images into memory
+            Bitmap finalImage = null;
+            padLen = padLen <= 4 ? 4 : padLen;
+            try
+            {
+                using (var bitmap = new Bitmap(imageFile))
+                {
+                    //create a bitmap to hold the combined image
+                    finalImage = new Bitmap(bitmap.Width + 2 * padLen, bitmap.Height + 2 * padLen);
+
+                    //get a graphics object from the image so we can draw on it
+                    using (Graphics g = Graphics.FromImage(finalImage))
+                    {
+                        //set background color
+                        g.Clear(Color.White);
+
+                        g.DrawImage(bitmap,
+                            new Rectangle(padLen, padLen, bitmap.Width, bitmap.Height));
+                    }
+                    bitmap.Dispose();
+                    return finalImage;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                if (finalImage != null)
+                    finalImage.Dispose();
+
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Helper method to quickly extract all images out of a PDF file
+        /// </summary>
+        public static void GetPdfImages(string pdfFileFullName, string outputDirectory = null)
+        {
+            var newFolderName = CheckPaths(pdfFileFullName, outputDirectory);
+            if (string.IsNullOrWhiteSpace(newFolderName) || !Directory.Exists(newFolderName))
+                return;
+            var images = new List<Image>();
+            using (var pdf = new PdfReader(pdfFileFullName))
+            {
+                for (var pageNumber = 1; pageNumber <= pdf.NumberOfPages; pageNumber++)
+                {
+                    var pg = pdf.GetPageN(pageNumber);
+                    images.AddRange(GetImagesFromPdfDict(pg, pdf));
+                }
+            }
+
+            for (var i = 0; i < images.Count; i++)
+            {
+                var img = images[i];
+                var imgPath = Path.Combine(newFolderName, $"Image-{i:0000}.bmp");
+                img.Save(imgPath);
+            }
+        }
+
+        internal static string CheckPaths(string pdfFileFullName, string outputDirectory = null)
+        {
+            if (String.IsNullOrEmpty(pdfFileFullName) || !File.Exists(pdfFileFullName))
+                return null;
+            outputDirectory =
+                outputDirectory ?? Path.GetDirectoryName(pdfFileFullName);
+            if (!Directory.Exists(outputDirectory))
+                return null;
+
+            var newFolderName = Path.GetFileNameWithoutExtension(pdfFileFullName);
+            newFolderName = Path.Combine(outputDirectory, newFolderName);
+            if (!Directory.Exists(newFolderName))
+                Directory.CreateDirectory(newFolderName);
+
+            return newFolderName;
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/802269/extract-images-using-itextsharp
+        /// </summary>
+        internal static IList<Image> GetImagesFromPdfDict(PdfDictionary dict, PdfReader doc)
+        {
+            var images = new List<Image>();
+            var res = (PdfDictionary)(PdfReader.GetPdfObject(dict.Get(PdfName.RESOURCES)));
+            var xobj = (PdfDictionary)(PdfReader.GetPdfObject(res.Get(PdfName.XOBJECT)));
+
+            if (xobj == null)
+                return images;
+            foreach (var name in xobj.Keys)
+            {
+                var obj = xobj.Get(name);
+                if (!obj.IsIndirect())
+                    continue;
+                var tg = (PdfDictionary)(PdfReader.GetPdfObject(obj));
+                var subtype = (PdfName)(PdfReader.GetPdfObject(tg.Get(PdfName.SUBTYPE)));
+                if (PdfName.IMAGE.Equals(subtype))
+                {
+                    var xrefIdx = ((PRIndirectReference)obj).Number;
+                    var pdfObj = doc.GetPdfObject(xrefIdx);
+                    var str = (PdfStream)(pdfObj);
+
+                    var pdfImage =
+                        new PdfImageObject((PRStream)str);
+                    var img = pdfImage.GetDrawingImage();
+
+                    images.Add(img);
+                }
+                else if (PdfName.FORM.Equals(subtype) || PdfName.GROUP.Equals(subtype))
+                {
+                    images.AddRange(GetImagesFromPdfDict(tg, doc));
+                }
+            }
+
+            return images;
         }
 
         /// <summary>
@@ -52,14 +257,14 @@ namespace NoFuture.Tokens
         public static string ToTextOnly(FileInfo fi)
         {
             var fullFileName = fi.FullName;
-            if (string.IsNullOrWhiteSpace(fullFileName) || !File.Exists(fullFileName))
+            if (String.IsNullOrWhiteSpace(fullFileName) || !File.Exists(fullFileName))
                 return fullFileName;
 
             var fileExtentsion = fi.Extension;
             var dirOut = fi.DirectoryName ??
                          Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            var fileOut = System.IO.Path.GetFileNameWithoutExtension(fullFileName);
-            var fullFileOut = System.IO.Path.Combine(dirOut, $"{fileOut}.txt");
+            var fileOut = Path.GetFileNameWithoutExtension(fullFileName);
+            var fullFileOut = Path.Combine(dirOut, $"{fileOut}.txt");
 
             switch (fileExtentsion)
             {
@@ -84,7 +289,7 @@ namespace NoFuture.Tokens
                     return fullFileOut;
             }
 
-            return string.Empty;
+            return String.Empty;
         }
 
         /// <summary>
@@ -94,7 +299,7 @@ namespace NoFuture.Tokens
         /// <returns></returns>
         public static HtmlDocument GetHtmlDocument(string rawHtml)
         {
-            if (string.IsNullOrWhiteSpace(rawHtml))
+            if (String.IsNullOrWhiteSpace(rawHtml))
                 return null;
 
             var html = new HtmlDocument
@@ -115,7 +320,7 @@ namespace NoFuture.Tokens
         /// <returns></returns>
         public static string GetHtmlOnly(string rawHtml)
         {
-            if (string.IsNullOrWhiteSpace(rawHtml))
+            if (String.IsNullOrWhiteSpace(rawHtml))
                 return null;
 
             var antlrRslts = AspNetParseTree.InvokeParse(new MemoryStream(Encoding.UTF8.GetBytes(rawHtml)));
@@ -130,11 +335,11 @@ namespace NoFuture.Tokens
         /// <returns></returns>
         public static XmlDocument GetHtmlAsXml(string rawHtml)
         {
-            if (string.IsNullOrWhiteSpace(rawHtml))
+            if (String.IsNullOrWhiteSpace(rawHtml))
                 return null;
 
             var htmlOnly = GetHtmlOnly(rawHtml);
-            if (string.IsNullOrWhiteSpace(htmlOnly))
+            if (String.IsNullOrWhiteSpace(htmlOnly))
                 return null;
             var xml = new XmlDocument();
             xml.LoadXml(htmlOnly);
@@ -149,7 +354,7 @@ namespace NoFuture.Tokens
         /// <returns></returns>
         public static dynamic GetHtmlAsJson(string rawHtml)
         {
-            if (string.IsNullOrWhiteSpace(rawHtml))
+            if (String.IsNullOrWhiteSpace(rawHtml))
                 return null;
             var xml = GetHtmlAsXml(rawHtml);
             if (xml == null)
@@ -169,7 +374,7 @@ namespace NoFuture.Tokens
         public static bool TryGetScriptBodies(string webResponseText, Func<string, bool> filter, out string[] scriptBodies)
         {
             scriptBodies = null;
-            if (string.IsNullOrWhiteSpace(webResponseText))
+            if (String.IsNullOrWhiteSpace(webResponseText))
                 return false;
 
             var ms = new MemoryStream(Encoding.UTF8.GetBytes(webResponseText));
