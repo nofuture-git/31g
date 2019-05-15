52dff0ad32522f7bbd08ddebe1e4e9b9f75e277b
NoFuture <brian.starrett@gmail.com>
2019-05-15 13:01:41 -0500

    missing save on .csproj

--- trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/DefenseTests/DefenseOfOtherTests/DuttonvHayesPupkoTests.cs
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/CriminalTests/DefenseTests/DefenseOfOtherTests/DuttonvHayesPupkoTests.cs
@@ -3,8 +3,6 @@ using NoFuture.Rand.Law.Criminal.US;
 using NoFuture.Rand.Law.Criminal.US.Defense;
 using NoFuture.Rand.Law.Criminal.US.Elements.Act;
 using NoFuture.Rand.Law.Criminal.US.Elements.Intent;
-using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
-using NoFuture.Rand.Law.US;
 using NoFuture.Rand.Law.US.Persons;
 using NUnit.Framework;
 
