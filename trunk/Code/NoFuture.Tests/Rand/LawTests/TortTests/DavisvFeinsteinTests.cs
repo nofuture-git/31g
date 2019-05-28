ae7a2554b51415a5466f65f761d7704baf8d9bd9
NoFuture <brian.starrett@gmail.com>
2019-05-28 10:56:11 -0500

    Nf.Rand.Law tort, added cases & concepts regarding reasonable care

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/DavisvFeinsteinTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements.ReasonableCare;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Davis v. Feinstein, 88 A.2d 695 (Pa. 1952)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, afflicted are considered to have met the standard
+    /// of care when using compensatory device. Not expected to
+    /// actually meet the same nimble results of the unafflicted - only to try.
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class DavisvFeinsteinTests
+    {
+        [Test]
+        public void DavisvFeinstein()
+        {
+            var test = new OfPhysicalDeficiency(ExtensionMethods.Plaintiff)
+            {
+                IsAfflictedWith = lp => lp is Davis,
+                IsAction = lp => lp is Davis,
+                IsVoluntary = lp => lp is Davis,
+                IsUsingCompensatoryDevice = lp => lp is Davis
+            };
+
+            var testResult = test.IsValid(new Davis(), new Feinstein());
+            Assert.IsTrue(testResult);
+
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class Davis : LegalPerson, IPlaintiff
+    {
+        public Davis(): base("Davis") { }
+    }
+
+    public class Feinstein : LegalPerson, ITortfeasor
+    {
+        public Feinstein(): base("Feinstein") { }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/SmithvSnellerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements.ReasonableCare;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Smith v. Sneller, 26 A.2d 452 (Pa. 1942)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, its not a greater duty of physical disability to meet the rational care standard
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class SmithvSnellerTests
+    {
+        [Test]
+        public void SmithvSneller()
+        {
+            var test = new OfPhysicalDeficiency(ExtensionMethods.Plaintiff)
+            {
+                IsAction = lp => lp is Smith,
+                IsVoluntary = lp => lp is Smith,
+                IsUsingCompensatoryDevice = lp => !(lp is Smith),
+                IsAfflictedWith = lp => lp is Smith
+            };
+
+            var testResult = test.IsValid(new Smith(), new Sneller());
+            Assert.IsFalse(testResult);
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class Smith : LegalPerson, IPlaintiff
+    {
+        public Smith(): base("Smith") { }
+    }
+
+    public class Sneller : LegalPerson, ITortfeasor
+    {
+        public Sneller(): base("Sneller") { }
+    }
+}
