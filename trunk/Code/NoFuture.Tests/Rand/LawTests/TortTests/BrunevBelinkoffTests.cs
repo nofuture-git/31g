79a6c91ef6fbcdf3f5b7befdd1857ccd4b322ce7
NoFuture <brian.starrett@gmail.com>
2019-05-31 15:46:57 -0500

    Nf.Rand.Law.Tort, another unit test on CustomsTerm

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/BrunevBelinkoffTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements;
+using NoFuture.Rand.Law.Tort.US.Terms;
+using NoFuture.Rand.Law.US.Property;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Brune v. Belinkoff, 235 N.E.2d 793 (Mass. 1968)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, claiming due care of custom when its not actually a custom
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class BrunevBelinkoffTests
+    {
+        [Test]
+        public void BrunevBelinkoff()
+        {
+            var test = new Negligence(ExtensionMethods.Tortfeasor)
+            {
+                SafetyConvention = new EightMgWithDex()
+                {
+                    //Belinkoff contents among his locality, court denies 
+                    IsPracticedByManyAmongGroup = false,
+                    IsConformedTo = lp => lp is Belinkoff
+                }
+            };
+
+            var testResult = test.IsValid(new Brune(), new Belinkoff());
+            Assert.IsTrue(testResult);
+
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class Brune : LegalPerson, IPlaintiff
+    {
+        public Brune(): base("Brune") { }
+    }
+
+    public class Belinkoff : LegalPerson, ITortfeasor
+    {
+        public Belinkoff(): base("Belinkoff") { }
+    }
+
+    public class EightMgWithDex : CustomsTerm
+    {
+
+    }
+}
