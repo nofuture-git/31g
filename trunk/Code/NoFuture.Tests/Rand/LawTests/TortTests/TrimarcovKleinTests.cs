180a7f59f47156c282723f76ee5df2684497b60f
NoFuture <brian.starrett@gmail.com>
2019-05-31 15:04:45 -0500

    Nf.Rand.Law.Tort, added terms for the concepts of Customs which form a legal framework for liability

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/TrimarcovKleinTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements;
+using NoFuture.Rand.Law.Tort.US.Terms;
+using NoFuture.Rand.Law.US.Property;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Trimarco v. Klein, 436 N.E.2d 502 (N.Y. 1982)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, sorting that a thing is a custom and that it is, indeed, breached
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class TrimarcovKleinTests
+    {
+        [Test]
+        public void TrimarcovKlein()
+        {
+            var test = new Negligence(ExtensionMethods.Tortfeasor)
+            {
+                SafetyConvention = new ReplaceWithShatterproofGlass()
+                {
+                    IsConformedTo = lp => !(lp is Klein)
+                }
+            };
+
+            var testResult = test.IsValid(new Trimarco(), new Klein());
+            Assert.IsTrue(testResult);
+
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class Trimarco : LegalPerson, IPlaintiff
+    {
+        public Trimarco(): base("Trimarco") { }
+    }
+
+    public class Klein : LegalPerson, ITortfeasor
+    {
+        public Klein(): base("Klein") { }
+    }
+
+    public class ReplaceWithShatterproofGlass : CustomsTerm
+    {
+
+    }
+}
