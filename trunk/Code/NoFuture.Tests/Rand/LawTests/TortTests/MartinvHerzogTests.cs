87f98aaf6462e3d894164edc2a522ce4fca41a1e
NoFuture <brian.starrett@gmail.com>
2019-06-03 12:44:58 -0500

    Nf.Rand.Law.Tort, added type for concept of negligence by statute

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/MartinvHerzogTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Martin v. Herzog, 19 N.E.2d 987 (N.Y. 1920)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, failure to follow command of statue means negligence by its standard
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class MartinvHerzogTests
+    {
+        [Test]
+        public void MartinvHerzog()
+        {
+            var test = new NegligenceByStatute(ExtensionMethods.Tortfeasor)
+            {
+                IsObeyStatute = lp => !(lp is Herzog),
+            };
+            var testResult = test.IsValid(new Martin(), new Herzog());
+            Assert.IsTrue(testResult);
+
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class Martin : LegalPerson, IPlaintiff
+    {
+        public Martin(): base("Martin") { }
+    }
+
+    public class Herzog : LegalPerson, ITortfeasor
+    {
+        public Herzog(): base("Herzog") { }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/TedlavEllmanTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Tedla v. Ellman, 19 N.E.2d 287 (N.Y. 1939)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, disobedience to a statue when doing so actually acheives its goal while obeying it does just the opposite
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class TedlavEllmanTests
+    {
+        [Test]
+        public void TedlavEllman()
+        {
+            var test = new NegligenceByStatute(ExtensionMethods.Tortfeasor)
+            {
+                IsObeyStatute = lp => !(lp is Ellman),
+                IsObedienceAddToDanger = lp => lp is Ellman
+            };
+
+            var testResult = test.IsValid(new Tedla(), new Ellman());
+            Assert.IsFalse(testResult);
+
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class Tedla : LegalPerson, IPlaintiff
+    {
+        public Tedla(): base("Tedla") { }
+    }
+
+    public class Ellman : LegalPerson, ITortfeasor
+    {
+        public Ellman(): base("Ellman") { }
+    }
+}
