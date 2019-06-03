9d52df83a68654cd07528091e94192a2a92b8e76
NoFuture <brian.starrett@gmail.com>
2019-06-03 15:04:35 -0500

    Nf.Rand.Law.Tort, more predicates related to NegligenceByStatute

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/GorrisvScottTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Gorris v. Scott, [1874] L.R. 9 Ex. 125
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, demo of non-negligence when attempting to use a statute's disobedience completely out of context
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class GorrisvScottTests
+    {
+        [Test]
+        public void GorrisvScott()
+        {
+            var test = new NegligenceByStatute(ExtensionMethods.Tortfeasor)
+            {
+                IsObeyStatute = lp => !(lp is Scott),
+                IsGroupMemberOfStatuesProtection = lp => !(lp is Scott) && !(lp is Gorris)
+            };
+
+            var testResult = test.IsValid(new Gorris(), new Scott());
+            Assert.IsFalse(testResult);
+
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class Gorris : LegalPerson, IPlaintiff
+    {
+        public Gorris(): base("Gorris") { }
+    }
+
+    public class Scott : LegalPerson, ITortfeasor
+    {
+        public Scott(): base("Scott") { }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/UhrvEastGreenbushCentralSchoolDistrictTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements;
+using NoFuture.Rand.Law.US.Property;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Uhr v. East Greenbush Central School District, 94 N.Y.2d 32 (1999)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// 
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class UhrvEastGreenbushCentralSchoolDistrictTests
+    {
+        [Test]
+        public void UhrvEastGreenbushCentralSchoolDistrict()
+        {
+            var test = new NegligenceByStatute(ExtensionMethods.Tortfeasor)
+            {
+                IsObeyStatute = lp => !(lp is EastGreenbushCentralSchoolDistrict),
+                IsGroupMemberOfStatuesProtection = lp => lp is Uhr || lp is EastGreenbushCentralSchoolDistrict,
+                IsDisobedienceCauseForAction = lp => false
+            };
+
+            var testResult = test.IsValid(new Uhr(), new EastGreenbushCentralSchoolDistrict());
+            Assert.IsFalse(testResult);
+
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class Uhr : LegalPerson, IPlaintiff
+    {
+        public Uhr(): base("Uhr") { }
+    }
+
+    public class EastGreenbushCentralSchoolDistrict : LegalPerson, ITortfeasor
+    {
+        public EastGreenbushCentralSchoolDistrict(): base("East Greenbush Central School District") { }
+    }
+}
