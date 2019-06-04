4225fec2768b9b7d92d278631fab450c33ec8f20
NoFuture <brian.starrett@gmail.com>
2019-06-04 15:13:21 -0500

    Nf.Rand.Law.Tort, added legal concepts and unit test of ContributoryNegligence and ComparativeNegligence

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/ColemanvSoccerAssnofColumbiaTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements;
+using NoFuture.Rand.Law.US.Property;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Coleman v. Soccer Ass’n. of Columbia, 432 Md. 679 (2013).
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// 
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class ColemanvSoccerAssnofColumbiaTests
+    {
+        [Test]
+        public void ColemanvSoccerAssnofColumbia()
+        {
+            var test = new ContributoryNegligence<SoccerFieldActivity>(ExtensionMethods.Tortfeasor)
+            {
+                GetContribution = lp =>
+                {
+                    if (lp is Coleman)
+                        return new SoccerFieldActivity(1);
+                    if (lp is SoccerAssnofColumbia)
+                        return new SoccerFieldActivity(10);
+                    return new SoccerFieldActivity(0);
+                }
+            };
+
+            var testResult = test.IsValid(new Coleman(), new SoccerAssnofColumbia());
+            Assert.IsFalse(testResult);
+            Console.WriteLine(test.ToString());
+
+        }
+    }
+
+    public class Coleman : LegalPerson, IPlaintiff
+    {
+        public Coleman(): base("Coleman") { }
+    }
+
+    public class SoccerAssnofColumbia : LegalPerson, ITortfeasor
+    {
+        public SoccerAssnofColumbia(): base("Soccer Ass’n. of Columbia") { }
+    }
+
+    public class SoccerFieldActivity : IRankable
+    {
+        private readonly int _rank;
+
+        public SoccerFieldActivity(int rank)
+        {
+            _rank = rank;
+        }
+
+        public int GetRank()
+        {
+            return _rank;
+        }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/NgaLivYellowCabCompanyofCaliforniaTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Li v. Yellow Cab Company of California, 532 P.2d 1226 (Cal. 1975)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, detailed explaination of comparative negligence
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class NgaLivYellowCabCompanyofCaliforniaTests
+    {
+        [Test]
+        public void NgaLivYellowCabCompanyofCalifornia()
+        {
+            var test = new ComparativeNegligence<DrivingCar>(ExtensionMethods.Tortfeasor)
+            {
+                GetContribution = lp =>
+                {
+                    if (lp is NgaLi)
+                        return new DrivingCar(1);
+                    if (lp is YellowCabCompanyofCalifornia)
+                        return new DrivingCar(2);
+                    return new DrivingCar(0);
+                }
+            };
+
+            var testResult = test.IsValid(new NgaLi(), new YellowCabCompanyofCalifornia());
+            Assert.IsTrue(testResult);
+
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class NgaLi : LegalPerson, IPlaintiff
+    {
+        public NgaLi(): base("Nga Li") { }
+    }
+
+    public class YellowCabCompanyofCalifornia : LegalPerson, ITortfeasor
+    {
+        public YellowCabCompanyofCalifornia(): base("Yellow Cab Company of California") { }
+    }
+
+    public class DrivingCar : IRankable
+    {
+        private readonly int _rank;
+
+        public DrivingCar(int rank)
+        {
+            _rank = rank;
+        }
+
+        public int GetRank()
+        {
+            return _rank;
+        }
+    }
+}
