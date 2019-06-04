570b7b52557a9764985254bc139a45b8c2a1ea59
NoFuture <brian.starrett@gmail.com>
2019-06-04 15:57:35 -0500

    Nf.Rand.Law.Tort, tort concept of assumed risk in Negligence

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/MurphyvSteeplechaseAmusementCoTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements;
+using NoFuture.Rand.Law.US.Property;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Murphy v. Steeplechase Amusement Co., 250 N.Y. 479 (N.Y. 1929)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, plaintiff clearly understood risk therefore no negligence
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class MurphyvSteeplechaseAmusementCoTests
+    {
+        [Test]
+        public void MurphyvSteeplechaseAmusementCo()
+        {
+            var test = new Negligence(ExtensionMethods.Tortfeasor)
+            {
+                IsNoDuty = lp => lp is SteeplechaseAmusementCo
+            };
+            var testResult = test.IsValid(new Murphy(), new SteeplechaseAmusementCo());
+            Assert.IsFalse(testResult);
+
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class Murphy : LegalPerson, IPlaintiff
+    {
+        public Murphy(): base("Murphy") { }
+    }
+
+    public class SteeplechaseAmusementCo : LegalPerson, ITortfeasor
+    {
+        public SteeplechaseAmusementCo(): base("Steeplechase Amusement Co.") { }
+    }
+}
