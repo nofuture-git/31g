afad2b3c23b5d5ab812fe0a627da307ed85da1c3
NoFuture <brian.starrett@gmail.com>
2019-06-10 09:28:30 -0500

    Nf.Rand.Law.Tort, added type for tort concept of emotional distress

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/DillonvLeggTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements;
+using NoFuture.Rand.Law.US.Property;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Dillon v. Legg, 441 P.2d 912 (Cal. 1968)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, defines a predicate test for emotional trauma
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class DillonvLeggTests
+    {
+        [Test]
+        public void DillonvLegg()
+        {
+            var test = new EmotionalDistress(ExtensionMethods.Tortfeasor)
+            {
+                IsByViolence = lp => lp is Legg,
+                IsCloselyRelated = (v, lp) => v is DaughterOfDillon && lp is Dillon,
+                IsDirectPrimaryWitness = lp => lp is Dillon,
+                IsNearSceneOfAccident = lp => lp is Dillon
+            };
+            var testResult = test.IsValid(new Legg(), new Dillon(), new DaughterOfDillon());
+            Assert.IsTrue(testResult);
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class Dillon : LegalPerson, IPlaintiff
+    {
+        public Dillon(): base("Dillon") { }
+    }
+
+    public class DaughterOfDillon : LegalPerson, IVictim
+    {
+        public DaughterOfDillon() : base("Erin Dillon") { }
+
+    }
+
+    public class Legg : LegalPerson, ITortfeasor
+    {
+        public Legg(): base("Legg") { }
+    }
+}
