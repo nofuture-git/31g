814fa420c7925ee0b1e1b7d5a12e8edc50e8a5cc
NoFuture <brian.starrett@gmail.com>
2019-06-19 16:06:37 -0500

    Nf.Rand.Law.Tort, added unit test for ProductStrictLiability,

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/TortTests/SpellervSearsRoebuckandCoTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US.Persons;
+using NoFuture.Rand.Law.Tort.US.Elements;
+using NoFuture.Rand.Law.US.Property;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.Tests
+{
+    /// <summary>
+    /// Speller v. Sears, Roebuck and Co., 790 N.E.2d 252 (N.Y. 2003)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, proving defect based on circumstantial evidence is enough for product strict liability
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class SpellervSearsRoebuckandCoTests
+    {
+        [Test]
+        public void SpellervSearsRoebuckandCo()
+        {
+            var test = new ProductStrictLiability(ExtensionMethods.Tortfeasor)
+            {
+                SubjectProperty = new Refrigerator(),
+                Injury = new Harm(ExtensionMethods.Plaintiff)
+                {
+                    IsDeath = lp => lp is Speller
+                },
+                IsForeseeable = p => p is Refrigerator,
+                //case doesn't specify this 
+                IsDirectCause = p => p is Refrigerator,
+                //only that circumstances are enough to proof this 
+                IsDefectiveAtTimeOfSale = p => p is Refrigerator
+            };
+
+            var testResult = test.IsValid(new Speller(), new SearsRoebuckandCo());
+            Assert.IsTrue(testResult);
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class Refrigerator : LegalProperty
+    {
+
+    }
+
+    public class Speller : LegalPerson, IPlaintiff
+    {
+        public Speller(): base("Speller") { }
+    }
+
+    public class SearsRoebuckandCo : LegalPerson, ITortfeasor
+    {
+        public SearsRoebuckandCo(): base("Sears, Roebuck and Co.") { }
+    }
+}
