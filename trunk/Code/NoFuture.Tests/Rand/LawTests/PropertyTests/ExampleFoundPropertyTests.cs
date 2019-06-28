c5d650a845cfc08d65aec7829289c5c9d18d5e7c
NoFuture <brian.starrett@gmail.com>
2019-06-28 16:38:56 -0500

    Nf.Rand.Law.Property, added types of Found property and base of unit test thereof, moved Consent up to Law root

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/PropertyTests/ExampleFoundPropertyTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFuture.Rand.Law.Property.US.Found;
+using NoFuture.Rand.Law.US;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Property.Tests
+{
+    [TestFixture]
+    public class ExampleFoundPropertyTests
+    {
+        private ILegalPerson _propertyOwner = new LegalPerson("Jim Owner");
+
+        [Test]
+        public void TestAbandonedProperty()
+        {
+            var test = new AbandonedProperty(ps => ps.FirstOrDefault(p => p.IsSamePerson(_propertyOwner)))
+            {
+
+            };
+        }
+    }
+}
