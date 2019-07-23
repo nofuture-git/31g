356f601a4eced2b6770c7c2cc39b63f5869a9c68
NoFuture <brian.starrett@gmail.com>
2019-07-23 14:45:08 -0500

    Nf.Rand.Law.Property, added another unit test for AdversePossession

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/PropertyTests/MarengoCaveCovRossTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using NoFuture.Rand.Law.US;
+using NoFuture.Rand.Law.Property.US.Acquisition;
+using NoFuture.Rand.Law.US.Persons;
+
+namespace NoFuture.Rand.Law.Property.Tests
+{
+    /// <summary>
+    /// Marengo Cave Co. v. Ross, 10 N.E.2d 917 (Ind. 1937)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// doctrine issue, spins around on the open and notorious part when the property is some underground cave
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class MarengoCaveCovRossTests
+    {
+        [Test]
+        public void MarengoCaveCovRoss()
+        {
+            var test = new AdversePossession()
+            {
+                SubjectProperty = new LegalProperty("part of 'Marengo Cave'")
+                {
+                    EntitledTo = new Ross(),
+                    InPossessionOf = new MarengoCaveCo()
+                },
+                Consent = Consent.NotGiven(),
+                EntitledOwnersAction = Act.DueDiligence(),
+                Inception = new DateTime(1908, 1, 1),
+                //time the case was written
+                Terminus = new DateTime(1937, 1, 1),
+                IsContinuousPossession = lp => true,
+                IsExclusivePossession = lp => true,
+                IsOpenNotoriousPossession = lp => false
+            };
+
+            var testResult = test.IsValid(new MarengoCaveCo(), new Ross());
+            Assert.IsFalse(testResult);
+            Console.WriteLine(test.ToString());
+        }
+    }
+
+    public class MarengoCaveCo : LegalPerson, IPlaintiff, IDisseisor
+    {
+        public MarengoCaveCo(): base("Marengo Cave Co.") { }
+    }
+
+    public class Ross : LegalPerson, IDefendant
+    {
+        public Ross(): base("Ross") { }
+    }
+}
