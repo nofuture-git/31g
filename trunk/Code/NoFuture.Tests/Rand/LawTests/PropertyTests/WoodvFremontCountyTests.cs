b03872801c454c6aba40ec9a6cefd2c5aabce7e4
NoFuture <brian.starrett@gmail.com>
2019-07-25 15:14:05 -0500

    Nf.Rand.Law.Property, added another unit test around cases about land interest

--- trunk/Code/NoFuture.Tests/Rand/LawTests/PropertyTests/WoodvFremontCountyTests.cs
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/PropertyTests/WoodvFremontCountyTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using NoFuture.Rand.Law.Property.US.FormsOf;
 using NoFuture.Rand.Law.Property.US.FormsOf.InTerra;
+using NoFuture.Rand.Law.Property.US.FormsOf.InTerra.Interests;
 using NoFuture.Rand.Law.US;
 using NoFuture.Rand.Law.US.Persons;
 using NUnit.Framework;
@@ -14,7 +15,7 @@ namespace NoFuture.Rand.Law.Property.Tests
     /// </summary>
     /// <remarks>
     /// <![CDATA[
-    /// 
+    /// doctrine issue, illustrates fee simple subject to conditional subsequent and fee simple determinable
     /// ]]>
     /// </remarks>
     [TestFixture]
@@ -23,16 +24,30 @@ namespace NoFuture.Rand.Law.Property.Tests
         [Test]
         public void WoodvFremontCounty()
         {
-            var test = new PropertyInterestFactory(new SomeLandInWyoming(), ExtensionMethods.Defendant);
+            var test00 = new FeeSimpleDeterminable(ExtensionMethods.Defendant)
+            {
+                IsSpecialLimitationPresent = lp => SomeLandInWyoming.WillExpireAutoIfNotUsedAsStated,
+            };
+            var testResult = test00.IsValid(new Wood(), new FremontCounty());
+            Assert.IsFalse(testResult);
+            Console.WriteLine(test00.ToString());
 
-            
+            var test01 = new FeeSimpleSubject2ConditionSubsequent(ExtensionMethods.Defendant)
+            {
+                IsRightOfEntry = lp => SomeLandInWyoming.IsStrictlyConditionalSubsequent
+            };
+
+            testResult = test01.IsValid(new Wood(), new FremontCounty());
+            Assert.IsFalse(testResult);
+            Console.WriteLine(test00.ToString());
 
         }
     }
 
     public class SomeLandInWyoming : RealProperty
     {
-
+        public static bool WillExpireAutoIfNotUsedAsStated => false;
+        public static bool IsStrictlyConditionalSubsequent => false;
     }
 
     public class Wood : LegalPerson, IPlaintiff
