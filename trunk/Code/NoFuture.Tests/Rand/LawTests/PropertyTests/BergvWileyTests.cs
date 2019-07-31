ecf6cdcf9844e1c03d4ca189b5a55ff9c96e3853
NoFuture <brian.starrett@gmail.com>
2019-07-31 16:12:13 -0500

    Nf.Rand.Law.Property, implemented some basics for Leasehold

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/PropertyTests/BergvWileyTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NoFuture.Rand.Law.Property.US.FormsOf.InTerra;
+using NoFuture.Rand.Law.Property.US.FormsOf.InTerra.Interests;
+using NoFuture.Rand.Law.US;
+using NoFuture.Rand.Law.US.Persons;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Property.Tests
+{
+    /// <summary>
+    /// Berg v. Wiley, 264 N.W.2d 145 (Minn. 1978).
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// 
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class BergvWileyTests
+    {
+        [Test]
+        public void BergvWiley()
+        {
+            var test = new Leasehold()
+            {
+                Inception = new DateTime(1970, 12, 1),
+                SubjectProperty = new RealProperty("building in Osseo, Minnesota"),
+                Terminus = new DateTime(1975, 12, 1)
+            };
+
+            var testResult = test.IsValid(new Berg(), new Wiley());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class Berg : LegalPerson, IPlaintiff, ILessee
+    {
+        public Berg(): base("Berg") { }
+    }
+
+    public class Wiley : LegalPerson, IDefendant, ILessor
+    {
+        public Wiley(): base("Wiley") { }
+    }
+}
