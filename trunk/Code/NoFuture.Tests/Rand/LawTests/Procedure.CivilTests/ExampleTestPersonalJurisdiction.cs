d00f2c52e108394ebc15613dc8141c7cf3a42032
NoFuture <brian.starrett@gmail.com>
2019-10-25 11:13:09 -0500

    added new Nf.Rand.Law project for the new text on Civil Procedure, added counterpart test project

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CivilTests/ExampleTestPersonalJurisdiction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using NoFuture.Rand.Law.Procedure.Civil.US.Jurisdiction;
+using NoFuture.Rand.Law.US;
+using NoFuture.Rand.Law.US.Persons;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Civil.Tests
+{
+    [TestFixture]
+    public class ExampleTestPersonalJurisdiction
+    {
+        [Test]
+        public void TestPersonalJurisdictionIsValid()
+        {
+            var testSubject = new PersonalJurisdiction()
+            {
+                IsDomicile = lp => lp is ExampleDefendant00
+            };
+
+            var testResult = testSubject.IsValid(new ExamplePlaintiff00(), new ExampleDefendant00());
+            Assert.IsTrue(testResult);
+            Console.WriteLine(testSubject.ToString());
+        }
+    }
+
+    public class ExamplePlaintiff00 : LegalPerson, IPlaintiff
+    {
+        public ExamplePlaintiff00() : base("Example P. Lantiff") { }
+    }
+
+    public class ExampleDefendant00 : LegalPerson, IDefendant
+    {
+        public ExampleDefendant00() : base("Example D. Fendant") {  }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CivilTests/Get-CivilProcedureUnitTestTemplate.ps1
@@ -0,0 +1,79 @@
+﻿function Get-CivilProcedureUnitTestTemplate
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true,position=0)]
+        [string] $Plaintiff,
+        [Parameter(Mandatory=$true,position=1)]
+        [string] $Defendant
+    )
+    Process
+    {
+        $safeNamePlaintiff = New-Object System.String(,[char[]]($Plaintiff.ToCharArray() | ? {[char]::IsLetterOrDigit($_)}))
+        $safeNameDefendant = New-Object System.String(,[char[]]($Defendant.ToCharArray() | ? {[char]::IsLetterOrDigit($_)}))
+
+        if([char]::IsDigit($safeNamePlaintiff.ToCharArray()[0])){
+            $safeNamePlaintiff = "_$safeNamePlaintiff"
+        }
+        
+        if([char]::IsDigit($safeNameDefendant.ToCharArray()[0])){
+            $safeNameDefendant = "_$safeNameDefendant"
+        }
+
+
+$someCode = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NoFuture.Rand.Law.US;
+using NoFuture.Rand.Law.US.Persons;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Civil.Tests
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// 
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class ${safeNamePlaintiff}v${safeNameDefendant}Tests
+    {
+        [Test]
+        public void ${safeNamePlaintiff}v${safeNameDefendant}()
+        {
+
+        }
+    }
+
+    public class ${safeNamePlaintiff} : LegalPerson, IPlaintiff
+    {
+        public ${safeNamePlaintiff}(): base("$Plaintiff") { }
+    }
+
+    public class ${safeNameDefendant} : LegalPerson, IDefendant
+    {
+        public ${safeNameDefendant}(): base("$Defendant") { }
+    }
+}
+
+"@
+        $filename = Join-Path (Get-Location).Path  ".\${safeNamePlaintiff}v${safeNameDefendant}Tests.cs"
+        [System.IO.File]::WriteAllText($filename, $someCode, [System.Text.Encoding]::UTF8)
+    }
+}
+
+$testPropertyDll = (Resolve-Path (".\bin\Debug\NoFuture.Rand.Law.Procedure.Civil.Tests.dll")).Path
+$nunit = (Resolve-Path ("..\..\..\..\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe"))
+
+function Test-NfRandLawCivilProcedureMethod($MethodName){
+    Invoke-Expression "$nunit $testPropertyDll --where `"method == $MethodName`""
+}
+
+function Test-NfRandLawCivilProcedure(){
+    Invoke-Expression "$nunit $testPropertyDll"
+}
