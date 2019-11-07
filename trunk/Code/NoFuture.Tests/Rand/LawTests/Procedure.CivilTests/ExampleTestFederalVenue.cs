4dd9369841dcf38aae5d82b68f9c37b566c841b9
NoFuture <brian.starrett@gmail.com>
2019-11-07 15:12:45 -0600

    added unit test for FederalVenue

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CivilTests/ExampleTestFederalVenue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NoFuture.Rand.Core;
+using NoFuture.Rand.Law.Procedure.Civil.US.Jurisdiction;
+using NoFuture.Rand.Law.US.Courts;
+using NoFuture.Rand.Law.US.Persons;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Civil.Tests
+{
+    [TestFixture]
+    public class ExampleTestFederalVenue
+    {
+        [Test]
+        public void TestFederalVenueIsValid()
+        {
+            var testSubject = new FederalVenue(new FederalCourt("District 01"))
+            {
+                GetDomicileLocation = lp =>
+                {
+                    if (lp is IPlaintiff)
+                        return new VocaBase("District 02");
+                    if (lp is IDefendant)
+                        return new VocaBase("District 08");
+                    return null;
+                },
+
+                GetInjuryLocation = lp => new VocaBase("District 01"),
+
+            };
+
+            var testResult = testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant());
+            Assert.IsTrue(testResult);
+            Console.WriteLine(testSubject.ToString());
+        }
+    }
+}
