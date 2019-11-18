fd4bc9a305e910a0ea2bde48a093f7350d1db5ef
NoFuture <brian.starrett@gmail.com>
2019-11-18 16:27:41 -0600

    Added some unit tests for ServiceOfProcess types

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CivilTests/ServiceOfProcessTests/ExampleTestInPersonDelivery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NoFuture.Rand.Law.Procedure.Civil.US.ServiceOfProcess;
+using NoFuture.Rand.Law.US.Courts;
+using NoFuture.Rand.Law.US.Persons;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Civil.Tests.ServiceOfProcessTests
+{
+    [TestFixture]
+    public class ExampleTestInPersonDelivery
+    {
+        [Test]
+        public void TestInPersonDeliveryIsValid()
+        {
+            var testSubject = new InPersonDelivery
+            {
+                Court = new StateCourt("UT"), IsAuthorizedPerson = lp => lp is ICourtOfficial,
+                GetDeliveredTo = lp => lp is ICourtOfficial ? new ExampleDefendant() : null,
+            };
+
+            var testResult = testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant(),
+                new ExampleLawEnforcement());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+        }
+    }
+
+    public class ExampleLawEnforcement : LegalPerson, ILawEnforcement
+    {
+        public ExampleLawEnforcement() :base("Johnny Law") { }
+    }
+}
---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CivilTests/ServiceOfProcessTests/ExampleTestVoluntaryEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFuture.Rand.Law.Procedure.Civil.US.ServiceOfProcess;
+using NoFuture.Rand.Law.US.Courts;
+using NoFuture.Rand.Law.US.Persons;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Civil.Tests.ServiceOfProcessTests
+{
+    [TestFixture]
+    public class ExampleTestVoluntaryEntry
+    {
+        [Test]
+        public void TestVoluntaryEntryIsValid()
+        {
+            var testSubject = new VoluntaryEntry
+            {
+                Court = new StateCourt("WV"), 
+                IsSigned = lp => lp is IDefendant || lp is INotaryPublic
+            };
+
+            var testResult =
+                testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant(), new ExampleNotaryPublic());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+        }
+
+    }
+
+    public class ExampleNotaryPublic : LegalPerson, INotaryPublic
+    {
+        public ExampleNotaryPublic() : base("Notia P. Ublic") { }
+    }
+}
