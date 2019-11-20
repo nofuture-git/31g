008d286fb3faf5d427034d15ace02713a2c5dbb8
NoFuture <brian.starrett@gmail.com>
2019-11-20 17:01:25 -0600

    added unit test for MotionToDismiss

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CivilTests/PleadingsTests/ExampleTestMotionToDismiss.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NoFuture.Rand.Law.Procedure.Civil.US.Jurisdiction;
+using NoFuture.Rand.Law.Procedure.Civil.US.Pleadings;
+using NoFuture.Rand.Law.US.Courts;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Civil.Tests.PleadingsTests
+{
+    [TestFixture]
+    public class ExampleTestMotionToDismiss
+    {
+        [Test]
+        public void TestMotionToDismissIsValid()
+        {
+            var testSubject = new MotionToDismiss
+            {
+                Court = new StateCourt("CO"),
+                GetCausesOfAction = lp => new ExampleCauseForAction(),
+                GetRequestedRelief = lp => new ExampleRequestRelief(),
+                IsReliefCanBeGranted = lc => lc is ExampleRequestRelief,
+                IsSigned = lp => true,
+                Jurisdiction = new ExampleJurisdictionIsFalse()
+            };
+
+            var testResult = testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+
+            testSubject = new MotionToDismiss
+            {
+                Court = new StateCourt("CO"),
+                GetCausesOfAction = lp => new ExampleCauseForAction(),
+                GetRequestedRelief = lp => new ExampleRequestRelief(),
+                IsReliefCanBeGranted = lc => false,
+                IsSigned = lp => true,
+                Jurisdiction = new ExampleJurisdictionIsTrue()
+            };
+
+            testResult = testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+
+            testSubject = new MotionToDismiss
+            {
+                Court = new StateCourt("CO"),
+                GetCausesOfAction = lp => new ExampleCauseForAction(),
+                GetRequestedRelief = lp => new ExampleRequestRelief(),
+                IsReliefCanBeGranted = lc => lc is ExampleRequestRelief,
+                IsSigned = lp => true,
+                Jurisdiction = new ExampleJurisdictionIsTrue()
+            };
+
+            testResult = testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsFalse(testResult);
+        }
+    }
+
+    public class ExampleJurisdictionIsFalse : LegalConcept, IJurisdiction
+    {
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            return false;
+        }
+
+        public ICourt Court { get; set; }
+    }
+
+    public class ExampleJurisdictionIsTrue : LegalConcept, IJurisdiction
+    {
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            return true;
+        }
+
+        public ICourt Court { get; set; }
+    }
+}
