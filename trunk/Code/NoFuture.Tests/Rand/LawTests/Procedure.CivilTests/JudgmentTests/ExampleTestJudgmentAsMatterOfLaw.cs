b74a79efcdb4ece60f58bb1da22a92f330e400ee
NoFuture <brian.starrett@gmail.com>
2019-11-26 13:29:03 -0600

    Nf.Rand.Law.Civil.Procedure, added another concept type for Judgment/JudgmentAsMatterOfLaw.cs with unit test

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CivilTests/JudgmentTests/ExampleTestJudgmentAsMatterOfLaw.cs
@@ -0,0 +1,26 @@
+﻿using NoFuture.Rand.Law.Procedure.Civil.US.Judgment;
+using NoFuture.Rand.Law.US;
+using NoFuture.Rand.Law.US.Courts;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Civil.Tests.JudgmentTests
+{
+    [TestFixture]
+    public class ExampleTestJudgmentAsMatterOfLaw
+    {
+        [Test]
+        public void TestJudgmentAsMatterOfLawIsValid()
+        {
+            var testSubject = new JudgmentAsMatterOfLaw()
+            {
+                GetSubjectPerson = lps => lps.Plaintiff(),
+                Court = new StateCourt("MS"),
+                GetAssertion = lp => new ExampleCauseForAction(),
+                IsCaseWeakBeyondReason = lc => lc is ExampleCauseForAction
+            };
+
+            var testResult = testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant());
+            Assert.IsTrue(testResult);
+        }
+    }
+}
