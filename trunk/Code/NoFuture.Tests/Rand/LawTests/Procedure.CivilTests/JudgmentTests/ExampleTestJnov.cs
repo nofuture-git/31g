5201093d8c7cf5352bdacf3ccce3be68223f78b1
NoFuture <brian.starrett@gmail.com>
2019-11-26 14:04:36 -0600

    Nf.Rand.Law.Civil.Procedure, added another concept type for Judgment/JudgmentNotwithstandingVerdict.cs with unit test

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CivilTests/JudgmentTests/ExampleTestJnov.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NoFuture.Rand.Law.Procedure.Civil.US.Judgment;
+using NoFuture.Rand.Law.US;
+using NoFuture.Rand.Law.US.Courts;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Civil.Tests.JudgmentTests
+{
+    [TestFixture]
+    public class ExampleTestJnov
+    {
+
+        [Test]
+        public void TestJnovIsValid()
+        {
+            var testSubject = new JudgmentNotwithstandingVerdict
+            {
+                Court = new StateCourt("LA"),
+                GetSubjectPerson = lps => lps.Plaintiff(),
+                GetAssertion = lp => new ExampleCauseForAction(),
+                IsCaseWeakBeyondReason = lp => lp is ExampleCauseForAction,
+                IsMadeMotionPriorToVerdict = lp => false
+            };
+
+            var testResult = testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsFalse(testResult);
+
+            testSubject.IsMadeMotionPriorToVerdict = lp => lp is ExamplePlaintiff;
+            testResult = testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant());
+            Assert.IsTrue(testResult);
+
+        }
+    }
+}
