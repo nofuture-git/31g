be4276db5e0a9bd4e132fc11278542313dff0a54
NoFuture <brian.starrett@gmail.com>
2019-11-26 14:38:29 -0600

    Nf.Rand.Law.Civil.Procedure, added another concept type for NewTrial with unit test

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CivilTests/JudgmentTests/ExampleTestNewTrial.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NoFuture.Rand.Law.Procedure.Civil.US.Judgment;
+using NoFuture.Rand.Law.US;
+using NoFuture.Rand.Law.US.Courts;
+using NoFuture.Rand.Law.US.Persons;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Civil.Tests.JudgmentTests
+{
+    [TestFixture]
+    public class ExampleTestNewTrial
+    {
+        [Test]
+        public void TestNewTrialIsValid00()
+        {
+            var testSubject = new NewTrial
+            {
+                Court = new StateCourt("TN"),
+                GetSubjectPerson = lps => lps.Plaintiff(),
+                GetAssertion = lp => new ExampleCauseForAction(),
+                IsCommittedProceduralError = lp => lp is ExampleCourtJudge
+            };
+            var testResult =
+                testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant(), new ExampleCourtJudge());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+
+        }
+
+        [Test]
+        public void TestNewTrialIsValid01()
+        {
+            var testSubject = new NewTrial
+            {
+                Court = new StateCourt("TN"),
+                GetSubjectPerson = lps => lps.Plaintiff(),
+                GetAssertion = lp => new ExampleCauseForAction(),
+                IsJurySeriouslyErroneousResult = lc => lc is ExampleCauseForAction
+            };
+            var testResult =
+                testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant(), new ExampleCourtJudge());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+
+        }
+
+        [Test]
+        public void TestNewTrialIsValid02()
+        {
+            var testSubject = new NewTrial
+            {
+                Court = new StateCourt("TN"),
+                GetSubjectPerson = lps => lps.Plaintiff(),
+                GetAssertion = lp => new ExampleCauseForAction(),
+                IsNeededToPreventInjustice = lc => lc is ExampleCauseForAction
+            };
+            var testResult =
+                testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant(), new ExampleCourtJudge());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+
+        }
+    }
+
+    public class ExampleCourtJudge : LegalPerson, ICourtOfficial
+    {
+        public ExampleCourtJudge(): base("Judge M.B. Judjinbad") { }
+    }
+}
