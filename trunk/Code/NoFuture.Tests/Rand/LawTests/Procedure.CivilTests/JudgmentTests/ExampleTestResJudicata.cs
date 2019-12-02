dc74d379257df8574c9bef52f85545ac1e7e0bb2
NoFuture <brian.starrett@gmail.com>
2019-12-02 12:06:22 -0600

    Nf.Rand.Law.Civil.Procedure, added another type for concept of Res Judicata

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CivilTests/JudgmentTests/ExampleTestResJudicata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NoFuture.Rand.Law.Procedure.Civil.US.Judgment;
+using NoFuture.Rand.Law.US.Courts;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Civil.Tests.JudgmentTests
+{
+    [TestFixture]
+    public class ExampleTestResJudicata
+    {
+        [Test]
+        public void TestResJudicataIsValid()
+        {
+            var testSubject = new ResJudicata
+            {
+                GetAssertion = lp => new ExampleCauseForAction(),
+                Court = new StateCourt("KS"),
+                IsFinalJudgment = lc => true,
+                IsJudgmentBasedOnMerits = lc => true,
+                IsSameClaim = lc => lc is ExampleCauseForAction,
+                IsSamePartyAsPrior = lp => lp is ExamplePlaintiff || lp is ExampleDefendant
+            };
+
+            var testResult = testSubject.IsValid(new ExamplePlaintiff(), new ExampleDefendant());
+            Assert.IsTrue(testResult);
+            Console.WriteLine(testSubject.ToString());
+        }
+    }
+}
