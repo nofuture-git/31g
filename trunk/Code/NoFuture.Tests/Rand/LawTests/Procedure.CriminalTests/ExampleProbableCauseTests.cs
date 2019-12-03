7e2f7285981616773ee2adbfc5f5b74014f5d6e8
NoFuture <brian.starrett@gmail.com>
2019-12-03 16:50:57 -0600

    Nf.Rand.Law.Criminal.Procedure, added new type for concept of ProbableCause with unit tests

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CriminalTests/ExampleProbableCauseTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using NoFuture.Rand.Law.Procedure.Criminal.US;
+using NoFuture.Rand.Law.US.Persons;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Criminal.Tests
+{
+    [TestFixture]
+    public class ExampleProbableCauseTests
+    {
+        [Test]
+        public void TestProbableCauseIsValid00()
+        {
+            var testSubject = new ProbableCause
+            {
+                IsReasonableConcludeCriminalActivity = lp => lp is ExampleLawEnforcement
+            };
+
+            var testResult = testSubject.IsValid(new ExampleSuspect(), new ExampleLawEnforcement());
+            Assert.IsTrue(testResult);
+            Console.WriteLine(testSubject.ToString());
+        }
+
+        [Test]
+        public void TestProbableCauseIsValid01()
+        {
+            var testSubject = new ProbableCause
+            {
+                GetInformationSource = lps => lps.FirstOrDefault(lp => lp is IInformant),
+                IsReasonableConcludeCriminalActivity = lp => lp is ExampleInformant
+            };
+
+            var testResult = testSubject.IsValid(
+                new ExampleSuspect(),
+                new ExampleInformant {IsInformationSufficientlyCredible = true, IsPersonSufficientlyCredible = true},
+                new ExampleLawEnforcement());
+            Assert.IsTrue(testResult);
+            Console.WriteLine(testSubject.ToString());
+        }
+    }
+
+    public class ExampleSuspect : LegalPerson, ISuspect
+    {
+        public ExampleSuspect() :base("Shaddy McShadison") { }
+    }
+
+    public class ExampleInformant : LegalPerson, IInformant
+    {
+        public ExampleInformant():base("Info Tweeker") { }
+
+        public bool IsPersonSufficientlyCredible { get; set; }
+        public bool IsInformationSufficientlyCredible { get; set; }
+    }
+}
