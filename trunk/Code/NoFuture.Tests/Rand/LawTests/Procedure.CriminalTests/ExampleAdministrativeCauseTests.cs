fefcd7febdd384cacfac272f2ecdfaf9bb873671
NoFuture <brian.starrett@gmail.com>
2019-12-04 16:29:18 -0600

    Nf.Rand.Law.Criminal.Procedure, added type for concept of AdministrativeCause

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CriminalTests/ExampleAdministrativeCauseTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NoFuture.Rand.Law.Procedure.Criminal.US;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Criminal.Tests
+{
+    [TestFixture]
+    public class ExampleAdministrativeCauseTests
+    {
+        [Test]
+        public void TestAdministrativeCauseIsValid00()
+        {
+            var testSubject = new AdministrativeCause<ExampleAdminSearchPolicy>
+            {
+                IsPolicyBased = t => t is ExampleAdminSearchPolicy,
+                IsToDetectCriminalActivity = t => false,
+                IsMinimumIntrusion = t => t is ExampleAdminSearchPolicy,
+                IsSpecificDetection = t => t is ExampleAdminSearchPolicy
+            };
+
+            var testResult = testSubject.IsValid();
+            Assert.IsTrue(testResult);
+            Console.WriteLine(testSubject.ToString());
+        }
+    }
+
+    public class ExampleAdminSearchPolicy : Rationale
+    {
+    }
+}
