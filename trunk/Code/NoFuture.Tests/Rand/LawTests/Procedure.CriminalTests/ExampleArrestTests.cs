1fbf5b54c822d4302e373a63f491b761b436394a
NoFuture <brian.starrett@gmail.com>
2019-12-05 17:18:59 -0600

    missing unit test from last commit

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CriminalTests/ExampleArrestTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using NoFuture.Rand.Law.Procedure.Criminal.US;
+using NoFuture.Rand.Law.Procedure.Criminal.US.Intrusions;
+using NoFuture.Rand.Law.US.Persons;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Criminal.Tests
+{
+    [TestFixture]
+    public class ExampleArrestTests
+    {
+        [Test]
+        public void TestArrestIsValid00()
+        {
+            var testSubject = new Arrest
+            {
+                IsAwareOfBeingArrested = lp => true,
+                ProbableCause = new ExampleProbableCause(),
+                IsOccurInPublicPlace = lp => true
+            };
+
+            var testResult = testSubject.IsValid(new ExampleSuspect(), new ExampleLawEnforcement());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+        }
+
+        [Test]
+        public void TestArrestIsValid01()
+        {
+            var testSubject = new Arrest
+            {
+                IsAwareOfBeingArrested = lp => true,
+                ProbableCause = new ExampleProbableCause(),
+                IsOccurInPublicPlace = lp => false,
+                Warrant = new ArrestWarrant
+                {
+                    GetObjectiveOfSearch = () => new ExampleSuspect(),
+                    IsObjectiveDescribedWithParticularity = lp => lp is ExampleSuspect,
+                    IsIssuerNeutralAndDetached = lp => lp is ExampleJudge,
+                    IsIssuerCapableDetermineProbableCause = lp => lp is ExampleJudge,
+                    GetIssuerOfWarrant = lps => lps.FirstOrDefault(lp => lp is IJudge)
+                }
+            };
+
+            var testResult = testSubject.IsValid(new ExampleSuspect(), new ExampleLawEnforcement(), new ExampleJudge());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+        }
+
+    }
+}
