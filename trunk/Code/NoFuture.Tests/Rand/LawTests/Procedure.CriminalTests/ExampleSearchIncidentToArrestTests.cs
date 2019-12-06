aaff547aac774a0cc52ae307e283f394d619a00b
NoFuture <brian.starrett@gmail.com>
2019-12-06 15:46:23 -0600

    Nf.Rand.Law.Criminal.Procedure, added new type for concept of SearchIncidentToArrestMotorVehicle, shuffled inheritance to keep only relevant predicates on the various types

--- trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CriminalTests/ExampleSearchIncidentToArrestTests.cs
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CriminalTests/ExampleSearchIncidentToArrestTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NoFuture.Rand.Law.Procedure.Criminal.US;
 using NoFuture.Rand.Law.Procedure.Criminal.US.Intrusions;
 using NUnit.Framework;
@@ -35,6 +32,67 @@ namespace NoFuture.Rand.Law.Procedure.Criminal.Tests
                 },
                 GetConductorOfSearch = lps => lps.FirstOrDefault(lp => lp is ExampleLawEnforcement),
                 
+            };
+
+            var testResult = testSubject.IsValid(new ExampleLawEnforcement(), new ExampleSuspect());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+        }
+
+        [Test]
+        public void TestSearchIncidentToArrestIsValid01()
+        {
+            var testSubject = new Search
+            {
+
+                ExpectationOfPrivacy = new SearchIncidentToArrestMotorVehicle()
+                {
+                    Arrest = new Arrest
+                    {
+                        IsAwareOfBeingArrested = lp => true,
+                        ProbableCause = new ExampleProbableCause(),
+                        IsOccurInPublicPlace = lp => true
+                    },
+                    IsArresteeNearPassengerCompartment = lp => lp is ExampleLawEnforcement,
+                    IsArresteeUnsecured = lp => lp is ExampleLawEnforcement,
+                    GetSubjectOfSearch = lps => lps.FirstOrDefault(lp => lp is ExampleSuspect),
+                },
+                InstrumentOfTheState = new InstrumentOfTheState
+                {
+                    IsAgentOfTheState = lp => true
+                },
+                GetConductorOfSearch = lps => lps.FirstOrDefault(lp => lp is ExampleLawEnforcement),
+
+            };
+
+            var testResult = testSubject.IsValid(new ExampleLawEnforcement(), new ExampleSuspect());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
+        }
+
+        [Test]
+        public void TestSearchIncidentToArrestIsValid02()
+        {
+            var testSubject = new Search
+            {
+
+                ExpectationOfPrivacy = new SearchIncidentToArrestMotorVehicle()
+                {
+                    Arrest = new Arrest
+                    {
+                        IsAwareOfBeingArrested = lp => true,
+                        ProbableCause = new ExampleProbableCause(),
+                        IsOccurInPublicPlace = lp => true
+                    },
+                    IsBeliefRelevantToCrimeEvidenceIsPresent = lp => lp is ExampleLawEnforcement,
+                    GetSubjectOfSearch = lps => lps.FirstOrDefault(lp => lp is ExampleSuspect)
+                },
+                InstrumentOfTheState = new InstrumentOfTheState
+                {
+                    IsAgentOfTheState = lp => true
+                },
+                GetConductorOfSearch = lps => lps.FirstOrDefault(lp => lp is ExampleLawEnforcement),
+
             };
 
             var testResult = testSubject.IsValid(new ExampleLawEnforcement(), new ExampleSuspect());
