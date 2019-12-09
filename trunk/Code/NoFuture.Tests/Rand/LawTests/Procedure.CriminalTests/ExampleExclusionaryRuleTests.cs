5bb81974fb289e0d9e212e3c8c58a9742008e628
NoFuture <brian.starrett@gmail.com>
2019-12-09 15:49:39 -0600

    Nf.Rand.Law.Criminal.Procedure, added types for concept of ExclusionaryRule.cs and moved ChallengeToProbableCause to the same folder

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/Procedure.CriminalTests/ExampleExclusionaryRuleTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NoFuture.Rand.Core;
+using NoFuture.Rand.Law.Procedure.Criminal.US.Challenges;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Procedure.Criminal.Tests
+{
+    [TestFixture]
+    public class ExampleExclusionaryRuleTests
+    {
+        [Test]
+        public void TestExclusionaryRuleIsValid00()
+        {
+            var testSubject = new ExclusionaryRule<IVoca>
+            {
+                GetEvidence = lp => lp is ExampleLawEnforcement ? new ExampleContraband() : null,
+                IsObtainedThroughUnlawfulMeans = v => false,
+                DerivativeExclusion = new DerivativeExclusionaryRule<IVoca>
+                {
+                    IsDerivedFromUnlawfulSource = v => true,
+                    IsInterveningEventsAttenuated = v => v is ExampleContraband
+                }
+            };
+
+            var testResult = testSubject.IsValid(new ExampleSuspect(), new ExampleLawEnforcement());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsFalse(testResult);
+
+        }
+    }
+}
