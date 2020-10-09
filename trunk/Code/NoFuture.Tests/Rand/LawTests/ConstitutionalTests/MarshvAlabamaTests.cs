e34ec26e5a664fb3e73214272003935dca6b5cf2
nofuture-git <brian.starrett@gmail.com>
2020-10-09 13:31:17 -0500

    start of study on US Constitutional law

---
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ConstitutionalTests/MarshvAlabamaTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using NoFuture.Rand.Law.US;
+using NoFuture.Rand.Law.US.Persons;
+using NUnit.Framework;
+
+namespace NoFuture.Rand.Law.Constitutional.Tests
+{
+    /// <summary>
+    /// Marsh v. Alabama 326 U.S. 501 (1946)
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// 
+    /// ]]>
+    /// </remarks>
+    [TestFixture]
+    public class MarshvAlabamaTests
+    {
+        [Test]
+        public void MarshvAlabama()
+        {
+
+        }
+    }
+
+    public class Marsh : LegalPerson, IPlaintiff
+    {
+        public Marsh(): base("Marsh") { }
+    }
+
+    public class Alabama : LegalPerson, IDefendant
+    {
+        public Alabama(): base("Alabama") { }
+    }
+}
