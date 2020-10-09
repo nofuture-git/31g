6cedfee5871b044202204c61d8ba88efffd5a208
nofuture-git <brian.starrett@gmail.com>
2020-10-09 16:37:48 -0500

    added first notes on Constitutional law

--- trunk/Code/NoFuture.Tests/Rand/LawTests/ConstitutionalTests/MarshvAlabamaTests.cs
+++ trunk/Code/NoFuture.Tests/Rand/LawTests/ConstitutionalTests/MarshvAlabamaTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using NoFuture.Rand.Law.Constitutional.US;
+using NoFuture.Rand.Law.Property.US.FormsOf.InTerra;
 using NoFuture.Rand.Law.US;
 using NoFuture.Rand.Law.US.Persons;
 using NUnit.Framework;
@@ -11,7 +13,8 @@ namespace NoFuture.Rand.Law.Constitutional.Tests
     /// </summary>
     /// <remarks>
     /// <![CDATA[
-    /// 
+    /// Doctrine issue, property which acts as a public community is protected regardless of
+    /// who has title
     /// ]]>
     /// </remarks>
     [TestFixture]
@@ -20,17 +23,48 @@ namespace NoFuture.Rand.Law.Constitutional.Tests
         [Test]
         public void MarshvAlabama()
         {
+            var testSubject = new StateAction()
+            {
+                Consent = Consent.NotGiven(),
+                GetActByPerson = p =>
+                {
+                    if (p is Marsh)
+                        return new DistributeReligiousLiterature()
+                        {
+                            IsAction = lp => true,
+                            IsVoluntary = lp => true
+                        };
 
+                    return null;
+                },
+                SubjectProperty = new TownOfChickasaw(),
+                IsProtectedRight = a => a is DistributeReligiousLiterature,
+                IsPublicCommunity = t => t is TownOfChickasaw
+            };
+
+            var testResult = testSubject.IsValid(new Alabama(), new Marsh());
+            Console.WriteLine(testSubject.ToString());
+            Assert.IsTrue(testResult);
         }
     }
 
-    public class Marsh : LegalPerson, IPlaintiff
+    public class Marsh : LegalPerson, IDefendant
     {
         public Marsh(): base("Marsh") { }
     }
 
-    public class Alabama : LegalPerson, IDefendant
+    public class Alabama : LegalPerson, IPlaintiff
     {
         public Alabama(): base("Alabama") { }
+    }
+
+    public class TownOfChickasaw : RealProperty
+    {
+        
+    }
+
+    public class DistributeReligiousLiterature : Act
+    {
+        
     }
 }
